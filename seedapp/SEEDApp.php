a146c4efda0a99ad19b07131e5152dc21bca1203
bobwildfong <bob@seeds.ca>
2017-10-02 20:24:01 -0400

    comment

--- seedapp/SEEDApp.php
+++ seedapp/SEEDApp.php
@@ -28,6 +28,15 @@ class SEEDApp_Worker
 }
 
 class SEEDApp_WorkerC extends SEEDApp_Worker
+// Since this is a SEEDApp_Worker it can be passed to any function that uses just a SEEDApp_Worker.
+// However, how does the console know about the oW? It would be better for this to be the console instead of just owning a console.
+
+// The right thing to do is to split Console01 into a) SEEDApp_WorkerC which knows about error messages, SVAs, current tabs and other "control" elements that are separate
+// from presentation; b) Console02 which knows how to draw tabsets, assemble HTML5 pages, etc.
+// Then SEEDApp_WorkerC can be passed around to logic code that wants to use SVAs and set error messages.
+// If you want to pass SEEDApp_WorkerC to code that uses SEEDApp_Worker, it's easy.
+// Console02 owns a SEEDApp_WorkerC
+// Other worker classes could derive from SEEDApp_WorkerC, and Console02 could own those instead and still be happy.
 {
     public $oC;
 
