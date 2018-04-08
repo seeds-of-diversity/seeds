b9ad9399ef41f22b4a5b468b350d2d3745c7dccf
bobwildfong <bob@seeds.ca>
2018-04-08 16:23:34 -0400

    lots of calendar stuff

--- seedlib/SEEDGoogleService.php
+++ seedlib/SEEDGoogleService.php
@@ -11,72 +11,98 @@ require_once( W_ROOT."os/google-api-php-client-2.2.1/vendor/autoload.php" );
 
 class SEEDGoogleService
 {
-    public  $client;
+    public  $client = null;
 
     private $raParms;
-    private $bCmdLine;
+    private $sErrMsg = "";
 
-    function __construct( $raParms, $bCmdLine = false )
+    function __construct( $raParms, $bConstructClient = true )
     {
         $this->raParms = $raParms;
-        $this->bCmdLine = $bCmdLine;
-        $this->client = $this->getClient( $raParms );
+        if( $bConstructClient ) {
+            // Probably the only time you wouldn't do this is when you're generating the credentials file (because getClient will fail without it)
+            $this->client = $this->getClient( $raParms );
+        }
     }
+
+    function GetErrMsg()  { return( $this->sErrMsg ); }
 
     /**
      * Returns an authorized API client.
      * @return Google_Client the authorized client object
-     *
-     * $bCmdLine : true = designed to be run on command line; false = designed to be run via browser
      */
-    private function getClient( $raParms )
+    function GetClient()
     {
-        $client = new Google_Client();
-        $client->setApplicationName( $raParms['application_name'] );
-        $client->setScopes( $raParms['scopes'] );
-        $client->setAuthConfig( $raParms['client_secret_file'] );
-        $client->setAccessType('offline');
-
-        $credentialsPath = $raParms['credentials_file'];
-
-        // Load previously authorized credentials from a file.
-        if (file_exists($credentialsPath)) {
-            $accessToken = json_decode(file_get_contents($credentialsPath), true);
-        } else {
-            // Credentials have not been created so request them from the user (command line tool only)
-            if( $this->bCmdLine ) {
-                // Request authorization from the user.
-                $authUrl = $client->createAuthUrl();
-                printf("Open the following link in your browser:\n%s\n", $authUrl);
-                print 'Enter verification code: ';
-                $authCode = trim(fgets(STDIN));
-
-                // Exchange authorization code for an access token.
-                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
-                // Store the credentials to disk.
-                if(!file_exists(dirname($credentialsPath))) {
-                    mkdir(dirname($credentialsPath), 0700, true);
-                }
-                file_put_contents($credentialsPath, json_encode($accessToken));
-                printf("Credentials saved to %s\n", $credentialsPath);
-            } else {
-                die( "Run getcreds.php to create the credentials file" );
-            }
+        if( !file_exists($this->raParms['client_secret_file']) ) {
+            $this->sErrMsg = "SEEDGoogleService: you have to install the google-client-secret file to access Google API";
+            goto done;
+        }
+        if( !file_exists($this->raParms['credentials_file']) ) {
+            $this->sErrMsg = "Please generate credentials for this user's calendar using the Admin console";
+            goto done;
         }
 
-        $client->setAccessToken($accessToken);
+        $this->initClient();
+
+        $accessToken = json_decode( file_get_contents($this->raParms['credentials_file']), true );
+
+        $this->setAccess( $accessToken );
+
+        done:
+        return( $this->client );
+    }
+
+    function GetCredentials( $bCmdLine )
+    /***********************************
+        Credentials have not been created so request them from the user
+     */
+    {
+        $this->initClient();
+
+        // Request authorization from the user.
+        $authUrl = $this->client->createAuthUrl();
+
+        if( $bCmdLine ) {
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+        } else {
+            // get it another way
+        }
+
+        // Exchange authorization code for an access token.
+        $accessToken = $this->client->fetchAccessTokenWithAuthCode( $authCode );
+
+        // Store the credentials to disk.
+//        if(!file_exists(dirname($credentialsPath))) {
+//            mkdir(dirname($credentialsPath), 0700, true);
+//        }
+        file_put_contents( $this->raParms['credentials_file'], json_encode($accessToken) );
+//        printf("Credentials saved to %s\n", $credentialsPath);
+
+        $this->setAccess( $accessToken );
+    }
+
+    private function initClient()
+    {
+        $this->client = new Google_Client();
+        $this->client->setApplicationName( $this->raParms['application_name'] );
+        $this->client->setScopes( $this->raParms['scopes'] );
+        $this->client->setAuthConfig( $this->raParms['client_secret_file'] );   // provided by Google API to identify us for quota purposes
+        $this->client->setAccessType('offline');
+    }
+
+    private function setAccess( $accessToken )
+    {
+        $this->client->setAccessToken( $accessToken );
 
         // Refresh the token if it's expired.
         // This was designed for command line use - see getcreds - will not work via browser unless the directory and file have write permission for apache
-        if ($client->isAccessTokenExpired()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+        if( $this->client->isAccessTokenExpired() ) {
+            $this->client->fetchAccessTokenWithRefreshToken( $this->client->getRefreshToken() );
+            file_put_contents($this->raParms['credentials_file'], json_encode($this->client->getAccessToken()) );
         }
-
-        return $client;
     }
-
 }
 
 ?>
