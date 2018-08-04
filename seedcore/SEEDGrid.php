1b536b9e7f53b7a546f0106093fe29b07735eec1
bobwildfong <bob@seeds.ca>
2018-08-03 23:33:43 -0400

    Row() instead of DrawRow()

--- seedcore/SEEDGrid.php
+++ seedcore/SEEDGrid.php
@@ -19,7 +19,7 @@ class SEEDBootstrapGrid
         $this->raParms = $raParms;
     }
 
-    function DrawRow( $c1, $c2 = null, $c3 = null, $c4 = null, $c5 = null, $c6 = null, $c7 = null, $c8 = null, $c9 = null, $c10 = null, $c11 = null, $c12 = null )
+    function Row( $c1, $c2 = null, $c3 = null, $c4 = null, $c5 = null, $c6 = null, $c7 = null, $c8 = null, $c9 = null, $c10 = null, $c11 = null, $c12 = null )
     {
         $s = "<div class='row'>"
             ."<div class='{$this->raParms['classCol1']}'>$c1</div>"
