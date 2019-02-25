4e4f9ee4dc3631024568de743cafca139a05bf66
bobwildfong <bob@seeds.ca>
2019-02-24 19:28:50 -0500

    data validation in MSDOffice

--- seedlib/msd/msdlib.php
+++ seedlib/msd/msdlib.php
@@ -66,23 +66,17 @@ class MSDLib
         return( $s );
     }
 
-    function AdminIntegrityTests()
+
+    function AdminIntegrityTests()  { return( $this->adminTests( 'integ_' ) ); }
+    function AdminWorkflowTests()   { return( $this->adminTests( 'workflow_' ) ); }
+    function AdminDataTests()       { return( $this->adminTests( 'data_' ) ); }
+
+    private function adminTests( $sPrefix )
     {
         $s = "";
 
         $oSPS = new SEEDProblemSolver( $this->spsDefs(), ['kfdb'=>$this->oMSDCore->oApp->kfdb] );
-        $ok = $oSPS->DoTests( 'integ_' );    // default second parm gives boolean return
-        $s .= $oSPS->GetOutput();
-
-        return( $s );
-    }
-
-    function AdminWorkflowTests()
-    {
-        $s = "";
-
-        $oSPS = new SEEDProblemSolver( $this->spsDefs(), ['kfdb'=>$this->oMSDCore->oApp->kfdb] );
-        $ok = $oSPS->DoTests( 'workflow_' );    // default second parm gives boolean return
+        $ok = $oSPS->DoTests( $sPrefix );    // default second parm gives boolean return
         $s .= $oSPS->GetOutput();
 
         return( $s );
@@ -91,7 +85,7 @@ class MSDLib
     private function spsDefs()
     {
         $sGNoSkipDel = "G._status='0'   AND NOT G.bSkip  AND NOT G.bDelete";
-        $sSActive    = "S._status='0'   AND S.eStatus='ACTIVE'";
+        $sSActive    = "S._status='0'   AND S.eStatus='ACTIVE' AND S.product_type='seeds'";
 
         $sSNoSkipDel = "S._status='0'   AND NOT S.bSkip  AND NOT S.bDelete";
         $sS1NoSkipDel = "S1._status='0' AND NOT S1.bSkip AND NOT S1.bDelete";
@@ -219,7 +213,8 @@ class MSDLib
                        'failLabel' => "Skipped growers have non-skipped seeds (solution: skip the seeds)",
                        'failShowRow' => "mbr_id [[m]] : [[mc]]",
                        'testSql' =>
-                           "SELECT G.mbr_id as m,G.mbr_code as mc FROM seeds.sed_curr_growers G, seeds.SEEDBasket_Products S "
+                           "SELECT G.mbr_id as m,G.mbr_code as mc "
+                          ."FROM seeds.sed_curr_growers G, seeds.SEEDBasket_Products S "
                           ."WHERE G.mbr_id=S.uid_seller AND S.product_type='seeds' AND G.bSkip AND S.eStatus NOT IN ('INACTIVE','DELETED') GROUP BY G.mbr_id,G.mbr_code",
                        'remedyLabel' => "Skip seeds for skipped growers",
                        'remedySql' =>
@@ -233,12 +228,13 @@ class MSDLib
                           'failLabel' => "Growers offering no seeds (solution: skip the growers)",
                           'failShowRow' => "mbr_id [[m]] : [[mc]]",
                           'testSql' =>
-                              "SELECT G.mbr_id as m, G.mbr_code as mc FROM seeds.sed_curr_growers G WHERE $sGNoSkipDel "
-                             ."AND NOT EXISTS (SELECT * FROM seeds.SEEDBasket_Products S WHERE G.mbr_id=S.uid_seller AND S.product_type='seeds' AND $sSActive)",
+                              "SELECT G.mbr_id as m, G.mbr_code as mc "
+                             ."FROM seeds.sed_curr_growers G WHERE $sGNoSkipDel "
+                             ."AND NOT EXISTS (SELECT * FROM seeds.SEEDBasket_Products S WHERE G.mbr_id=S.uid_seller AND $sSActive)",
                           'remedyLabel' => "Skip active growers who have no active seeds",
                           'remedySql' =>
                               "UPDATE seeds.sed_curr_growers G SET G.bSkip=1 WHERE $sGNoSkipDel "
-                             ."AND NOT EXISTS (SELECT * FROM seeds.SEEDBasket_Products S WHERE G.mbr_id=S.uid_seller AND S.product_type='seeds' AND $sSActive)",
+                             ."AND NOT EXISTS (SELECT * FROM seeds.SEEDBasket_Products S WHERE G.mbr_id=S.uid_seller AND $sSActive)",
                 ),
 
             /* Check that the data is normalized
@@ -253,25 +249,57 @@ class MSDLib
                            "SELECT G.mbr_id as m,G.mbr_code as mc FROM seeds.sed_curr_growers G WHERE LENGTH(G.mbr_code)<>8 AND G.mbr_code<>'SODC/SDPC'",
                      ),
 
+            'data_cat_sp_var_exist' =>
+                /* After this test you can always trust PxPE WHERE PE.k='cat/sp/var' to exist
+                 */
+                array( 'title' => "Check that every seed has a (category,species,variety) defined",
+                       'testType' => 'rows0',
+                       'failLabel' => "Seeds missing (cat,species,var) fields",
+                       'failShowRow' => "[[k]] is ([[cat]],[[sp]],[[var]])",
+                       'testSql' =>
+                           "SELECT P._key as k,PE1.v as cat,PE2.v as sp,PE3.v as var "
+                          ."FROM seeds.SEEDBasket_Products P LEFT JOIN seeds.SEEDBasket_ProdExtra PE1 "
+                               // yields NULL PE fields if category undefined or deleted
+                               ."ON (P._key=PE1.fk_SEEDBasket_Products AND PE1.k='category' AND PE1._status='0') "
+                               ."LEFT JOIN seeds.SEEDBasket_ProdExtra PE2 "
+                               ."ON (P._key=PE2.fk_SEEDBasket_Products AND PE2.k='species' AND PE2._status='0') "
+                               ."LEFT JOIN seeds.SEEDBasket_ProdExtra PE3 "
+                               ."ON (P._key=PE3.fk_SEEDBasket_Products AND PE3.k='variety' AND PE3._status='0') "
+                          // not limited to ACTIVE records because it shouldn't be
+                          ."WHERE P._status='0' AND P.product_type='seeds' AND "
+                          ."(PE1._key IS NULL OR PE2._key IS NULL OR PE3._key IS NULL)"
+                     ),
+
+
             'data_category_normal' =>
                 array( 'title' => "Check for non-standard categories",
                        'testType' => 'rows0',
                        'failLabel' => "Seeds have non-standard categories",
                        'failShowRow' => "[[n]] seeds have category '[[category]]'",
                        'testSql' =>
-                           "SELECT S.category as category,count(*) as n FROM seeds.sed_curr_seeds S "
-                          ."WHERE $sSNoSkipDel AND "
-                          ."category NOT IN ('FLOWERS AND WILDFLOWERS','FRUIT','GRAIN','HERBS AND MEDICINALS','TREES AND SHRUBS','VEGETABLES','MISC') "
-                          ."GROUP BY category",
+                           "SELECT PE.v as category,count(*) as n "
+                          ."FROM seeds.SEEDBasket_Products P, seeds.SEEDBasket_ProdExtra PE "
+                          ."WHERE P._key=PE.fk_SEEDBasket_Products AND PE.k='category' AND "
+                                // not limited to ACTIVE records because it shouldn't be
+                                ."P._status='0' AND PE._status='0' AND P.product_type='seeds' AND "
+                                ."PE.v NOT IN ('flowers','fruit','grain','herbs','trees','vegetables','misc') "
+                          ."GROUP BY 1",
                      ),
 
             'data_type_empty' =>
-                array( 'title' => "Check for blank Types (the only value that is not allowed)",
+                array( 'title' => "Check for blank Species",
                        'testType' => 'rows0',
-                       'failLabel' => "Seeds have blank Type",
+                       'failLabel' => "Seeds have blank Species",
                        'failShowRow' => "category=[[category]], variety=[[variety]]",
                        'testSql' =>
-                           "SELECT S.category as category,S.variety as variety FROM seeds.sed_curr_seeds S WHERE S.type='' and $sSNoSkipDel",
+                           "SELECT PE1.v as category,PE3.v as variety "
+                          ."FROM seeds.SEEDBasket_Products P,seeds.SEEDBasket_ProdExtra PE1,seeds.SEEDBasket_ProdExtra PE2,seeds.SEEDBasket_ProdExtra PE3 "
+                          ."WHERE P._key=PE1.fk_SEEDBasket_Products AND PE1.k='category' AND PE1._status='0' AND "
+                                ."P._key=PE2.fk_SEEDBasket_Products AND PE2.k='species' AND PE2._status='0' AND "
+                                ."P._key=PE3.fk_SEEDBasket_Products AND PE3.k='variety' AND PE3._status='0' AND "
+                                // not limited ACTIVE records because it shouldn't be
+                                ."P._status='0' AND P.product_type='seeds' AND "
+                                ."PE2.v=''"
                      ),
 
             'data_year_growers' =>
@@ -290,14 +318,30 @@ class MSDLib
                        'testType' => 'n0',
                        'failLabel' => "[[n]] rows in sed_curr_seeds that are neither bSkip nor bDelete, but don't have the current year",
                        'testSql' =>
-                           "SELECT count(*) FROM seeds.sed_curr_seeds S WHERE S.year<>'$yearCurrent' AND $sSNoSkipDel",
+                           "SELECT count(*) FROM seeds.SEEDBasket_Products P LEFT JOIN seeds.SEEDBasket_ProdExtra PE "
+                                               ."ON (P._key=PE.fk_SEEDBasket_Products AND PE.k='year' AND PE._status='0') "
+                          ."WHERE P._status='0' AND P.product_type='seeds' AND P.eStatus='ACTIVE' AND "
+                                ."(PE.v IS NULL OR PE.v<>'$yearCurrent')",
                        'remedyLabel' => "Set current year for seeds",
                        'remedySql' =>
-                           "UPDATE seeds.sed_curr_seeds S SET S.year='$yearCurrent' WHERE $sSNoSkipDel",
+                           // update PE.v where exists but not current year
+                           "UPDATE seeds.SEEDBasket_Products P LEFT JOIN seeds.SEEDBasket_ProdExtra PE "
+                                               ."ON (P._key=PE.fk_SEEDBasket_Products AND PE.k='year' AND PE._status='0') "
+                          ."SET PE.v='$yearCurrent' "
+                          ."WHERE P._status='0' AND P.product_type='seeds' AND P.eStatus='ACTIVE' AND "
+                                ."PE._key IS NOT NULL AND PE.v<>'$yearCurrent' ;"
+                          // insert PE because the year row is missing
+                          ."INSERT INTO seeds.SEEDBasket_ProdExtra (fk_SEEDBasket_Products,k,v) "
+                          ."SELECT P._key,'year','$yearCurrent' "
+                          ."FROM seeds.SEEDBasket_Products P LEFT JOIN seeds.SEEDBasket_ProdExtra PE "
+                          ."ON (P._key=PE.fk_SEEDBasket_Products AND PE.k='year' AND PE._status='0') "
+                          ."WHERE P._status='0' AND P.product_type='seeds' AND P.eStatus='ACTIVE' AND "
+                                ."PE._key IS NULL",
+                        'bNonFatal' => true     // remove this later
                      ),
 
             'data_count_nTotal' =>
-                array( 'title' => "Check that the grower seed-count totals equal sums of category counts",
+                array( 'title' => "Check that the grower seed-count totals add up per grower",
                        'testType' => 'rows0',
                        'failLabel' => "Grower total count does not match sum of Flower,Fruit,Grain,Herb,Tree,Veg,Misc counts",
                        'failShowRow' => "[[m]] : [[mc]]",
@@ -314,7 +358,7 @@ class MSDLib
                        'testSql' =>
                            "SELECT G.mbr_id as m, G.mbr_code as mc, G.nTotal as nTotal FROM seeds.sed_curr_growers G "
                               ."WHERE $sGNoSkipDel AND "
-                              ."G.nTotal <> (SELECT count(*) FROM seeds.sed_curr_seeds S WHERE S.mbr_id=G.mbr_id AND $sSNoSkipDel)",
+                              ."G.nTotal <> (SELECT count(*) FROM seeds.SEEDBasket_Products S WHERE S.uid_seller=G.mbr_id AND $sSActive)",
                      ),
 
             'data_count_sumsGandS' =>
@@ -323,7 +367,7 @@ class MSDLib
                        'failLabel' => "Sum of grower totals - count of seeds = [[n]]",
                        'testSql' =>
                            "SELECT (SELECT sum(G.nTotal) FROM seeds.sed_curr_growers G WHERE $sGNoSkipDel) "
-                              ." - (SELECT count(*) FROM seeds.sed_curr_seeds S WHERE $sSNoSkipDel) as n",
+                              ." - (SELECT count(*) FROM seeds.SEEDBasket_Products S WHERE $sSActive) as n",
                      ),
 
 
