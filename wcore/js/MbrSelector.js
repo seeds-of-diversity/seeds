1d3938011792cb545fb1b30ebe0314cf267cba4a
bob <bob@localhost>
2020-01-15 16:11:29 -0500

    MbrSelector() and class SFU_TextComplete

---
+++ wcore/js/MbrSelector.js
@@ -0,0 +1,65 @@
+// requires SEEDCore.js
+// requires SFUTextComplete.js
+
+/* Selector tool for choosing a contact from MbrContacts
+
+   Usage:
+       let o = new MbrSelector( { idTxtSearch : 'myDummyTxt',
+                                  idOutReport : 'myReport',
+                                  idOutKey    : 'myMbrKey' } );
+
+       <div style='position:relative'>
+           <input type='text' id='myDummyTxt'/>       // you type here
+           <span id='myReport'> </span>               // details of your choice go here
+           <input type='hidden' id='myMbrKey'/>       // your selected mbr key goes here
+       </div>
+*/
+class MbrSelector extends SFU_TextComplete
+{
+    constructor( raConfig )
+    {
+        super(raConfig.idTxtSearch);
+        this.raConfig = raConfig;
+        this.mbrData = [];          // store the mbr lookup data here for reporting when a result is chosen
+    }
+
+    GetMatches( sSearch )
+    {
+        let raRet = [];
+
+        let jxData = { qcmd    : 'mbr-search',
+                       lang    : "EN",
+                       sSearch : sSearch
+                     };
+        let o = SEEDJXSync( urlQ, jxData );
+        if( !o || !o['bOk'] || !o['raOut'] ) {
+            alert( "Sorry there is a server problem" );
+        } else {
+            this.mbrData = o['raOut'];   // save this so we can look it up in ResultChosen
+            for( let i = 0; i < o['raOut'].length; ++i ) {
+                let r = o['raOut'][i];
+                raRet[i] = { val: r['_key'], label: this.makelabel(r) };
+            }
+        }
+        return( raRet );
+    }
+
+    ResultChosen( val )
+    {
+        for( let i = 0; i < this.mbrData.length; ++i ) {
+            let r = this.mbrData[i];
+            if( r['_key'] == val ) {
+                $("#"+this.raConfig['idOutReport']).html( this.makelabel(r) );
+                $("#"+this.raConfig['idOutKey']).val( r['_key'] );
+                break;
+            }
+        }
+    }
+
+    makelabel( r )
+    {
+        let l = r['firstname']+" "+r['lastname']+" ("+r['_key']+")";
+        if( r['city'] )  l += " in "+r['city'];
+        return( l );
+    }
+}
--- wcore/js/SFUTextComplete.js
+++ wcore/js/SFUTextComplete.js
@@ -3,80 +3,92 @@
    Make a text input that creates a <select> containing search matches.
    There can be multiple TextComplete controls because they are defined per id.
 
-   Use SFU_TextCompleteVars to define the TextComplete controls and their handlers.
-   { 'id_of_SFU_TextComplete' : { // Called when someone types in the text control. Return the <option>s that match the search text.
-                                  'fnFillSelect' : function (sSearch) { return [ [val, label], [val, label] ] },
-                                  // Called when someone clicks on an <option>
-                                  'fnSelectChoose' : function (val) { } 
+   Usage:
+       <div style='position:relative'>        // this is necessary for positioning the <select>
+       <input type='text' id='myTC'/>
+       </div>
+       
+       class myTextComplete extends SFU_TextComplete { 
+           constructor( idTextElement )  { super(idTextElement); }
+           GetMatches( sSearch )         { return array of opts for the <select> [ [val, label], [val, label] ] }
+           ResultChosen( v )             { do what you want with the chosen value }
+       }
+       let o = new myTextComplete('myTC');
    }
  */
-var SFU_TextCompleteVars = {};
 
-$(document).ready( function() {
-    SFU_TextComplete_Init();
-});
-
-function SFU_TextComplete_Init()
-/*******************************
-    Called by document.ready but you have to call this again if you create a node containing a .SFU_TextComplete
- */
+class SFU_TextComplete
 {
-    // Don't set up the TextComplete if there isn't one. offset().left below causes a js error that kills other ready() functions
-    if( typeof $('.SFU_TextComplete').offset() == 'undefined' ) { console.log("No .SFU_TextComplete"); return; }
+    constructor( idTextElement )
+    {
+        this.idTextElement = idTextElement;
+        this.Init();
+    }
 
-    $('.SFU_TextComplete').click( function(e) { 
-        let oTC = $(this);   // preserve $this in the closure below
+    Init()
+    {
+        let saveThis = this;
 
-        // Get the <select> that was created on a previous click or create it if it doesn't exist
-        let select = $(this).siblings('.SFUACSelect');
-        if( !select.length ) {
-            select = $("<select class='SFUACSelect'><option>--- Type more ---</option></select>").insertAfter($(this));
+        // When someone clicks the text element, create a <select> element (if not already existing) and position it beneath
+        $('#'+this.idTextElement).click( function(e) { 
+            let select = $(this).siblings('.SFUTCSelect');
+            if( !select.length ) {
+                // Create the <select> and position it
+                select = $("<select class='SFUTCSelect'><option>--- Type more ---</option></select>").insertAfter($(this));
 
-            let xSearch = $(this).offset().left;
-            let ySearch = $(this).offset().top;
-            let hSearch = $(this).outerHeight();
-            let xAnchor = $(this).parent().offset().left;
-            let yAnchor = $(this).parent().offset().top;
-            select.css({ position:'absolute', left:xSearch-xAnchor, top:ySearch-yAnchor+hSearch+1, 'z-index':2 });
-            
-            // When someone clicks on an <option> send its value to fnSelectChoose and remove the <select> 
-            select.click( function(e) {
-                e.preventDefault(); 
-                let v = SFU_TextCompleteVars[oTC.attr('id')];
-                if( typeof v != 'undefined' ) {
-                    oTC.val( (v['fnSelectChoose'])( $(this).val() ) ); 
+                let xSearch = $(this).offset().left;
+                let ySearch = $(this).offset().top;
+                let hSearch = $(this).outerHeight();
+                let xAnchor = $(this).parent().offset().left;
+                let yAnchor = $(this).parent().offset().top;
+                select.css({ position:'absolute', left:xSearch-xAnchor, top:ySearch-yAnchor+hSearch+1, 'z-index':2 });
+                
+                // When someone clicks on an <option> send its value to this.ResultChosen() and remove the <select> 
+                let thisText = $(this);
+                select.click( function(e) {
+                    e.preventDefault(); 
+                    saveThis.ResultChosen( $(this).val() );
                     $(this).remove(); 
-                }
-            });
-        }
-    });
-    
-    // In someone has typed at least 3 chars in the text input, use the callback to fetch a set of <options> that match
-    $('.SFU_TextComplete').keyup( function(e) { 
-        let srchVal = $(this).val();
-        if( srchVal.length < 3 )  return;
-        
-        let select = $(this).siblings('.SFUACSelect');
-        if( !select.length ) return;
-
-        // remove all <option>s from the <select>
-        select.find('option').each(function() { $(this).remove(); });
-        
-        // call the defined function to get new options
-        let nOpts = 0;
-        let v = SFU_TextCompleteVars[$(this).attr('id')];
-        if( typeof v != 'undefined' ) {
-            let options = (v['fnFillSelect'])(srchVal);
-            // limit the number of options to 20 because you can keep typing to get a better match
-            nOpts = options.length;
-            if( nOpts > 20 ) nOpts = 20;
-            for( let i = 0; i < nOpts; ++i ) {
-                let r = options[i];
-                select.append($('<option>', { value: r['val'], text: r['label'] }));
+                    thisText.val('');
+                });
             }
-        }
+        });
 
-        // make the select control tall enough to contain all options
-        select.attr({ size: nOpts }); 
-    });
+        // If someone has typed at least 3 chars in the text input, fetch a set of <options> that match
+        $('#'+this.idTextElement).keyup( function(e) { 
+            let sSearch = $(this).val();
+            if( sSearch.length < 3 )  return;
+
+            let select = $(this).siblings('.SFUTCSelect');
+            if( !select.length ) return;
+
+            // remove all <option>s from the <select>
+            select.find('option').each(function() { $(this).remove(); });
+
+            // get new options
+	        let nOpts = 0;
+	            let options = saveThis.GetMatches( sSearch );
+	            // limit the number of options to 20 because you can keep typing to get a better match
+	            nOpts = options.length;
+	            if( nOpts > 20 ) nOpts = 20;
+	            for( let i = 0; i < nOpts; ++i ) {
+	                let r = options[i];
+	                select.append($('<option>', { value: r['val'], text: r['label'] }));
+	            }
+	        
+	
+	        // make the select control tall enough to contain all options
+	        select.attr({ size: nOpts }); 
+	    });
+	}
+    
+    GetMatches( sSearch )
+    {
+        // OVERRIDE THIS to return [[val,label],[val,label],...] for the <option>s that match the search
+    }
+    
+    ResultChosen( val )
+    {
+        // OVERRIDE THIS to do what you want when the user clicks on the given <option>
+    }
 }
