15d9ae254d8c1f3da3437f884133b7ae4d657f49
bob <bob@localhost>
2020-05-20 19:55:14 -0400

    try to fix case where kCurr and iCurr become unsynchronized

--- seedcore/SEEDUI.php
+++ seedcore/SEEDUI.php
@@ -936,19 +936,6 @@ var_dump("k:".$this->oComp->Get_kCurr()." i:".$this->oComp->Get_iCurr()." iWO:".
             if( ($raRow = $this->GetRowData( $this->oComp->Get_iCurr() )) ) {
                 $this->oComp->Set_kCurr( @$raRow['_key'] ?: 0 );
             }
-
-            /* MAYBE OBSOLETE
-             * There was a weird case that made the view shrink smaller than iCurr. It might not happen anymore, but this was the fix.
-             * It's necessary to put it here because nViewSize is not known until some view data is fetched, which might not happen
-             * before the GetRowData() above.
-             */
-            if( $this->oComp->Get_iCurr() >= $this->nViewSize ) {
-                $this->oComp->Set_iCurr(0);
-                $this->oComp->Set_iWindowOffset(0);
-                if( ($raRow = $this->GetRowData( $this->oComp->Get_iCurr() )) ) {
-                    $this->oComp->Set_kCurr( @$raRow['_key'] ?: 0 );
-                }
-            }
         }
 
 
@@ -972,6 +959,21 @@ var_dump("k:".$this->oComp->Get_kCurr()." i:".$this->oComp->Get_iCurr()." iWO:".
             // reposition iWO to include the new iCurr row
             $this->oComp->Set_iWindowOffset( $this->IdealWindowOffset() );
         }
+
+
+        /* There is a weird case when a relogin is necessary after a session timeout where kCurr and iCurr seem unsynchronized.
+         * At this point, the iCurr row might as well be loaded, so do that and test that it contains kCurr. If not, reset the view.
+         */
+        if( !($raRow = $this->GetRowData( $this->oComp->Get_iCurr() )) || $raRow['_key'] != $this->oComp->Get_kCurr() ) {
+            var_dump("sync problem fixed: k:".$this->oComp->Get_kCurr()." i:".$this->oComp->Get_iCurr()." iWO:".$this->oComp->Get_iWindowOffset());
+            $this->oComp->Set_iCurr(0);
+            $this->oComp->Set_iWindowOffset(0);
+            if( ($raRow = $this->GetRowData( $this->oComp->Get_iCurr() )) ) {
+                $this->oComp->Set_kCurr( @$raRow['_key'] ?: 0 );
+            }
+        }
+
+
 
 //if( in_array($this->oComp->oUI->oApp->sess->GetUID(), [1,1499]) )
 var_dump("k:".$this->oComp->Get_kCurr()." i:".$this->oComp->Get_iCurr()." iWO:".$this->oComp->Get_iWindowOffset());
