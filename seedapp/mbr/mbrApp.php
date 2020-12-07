e9fb028123d71ae35b7445e90f320b7a73c7fc31
bobwildfong <bob@seeds.ca>
2020-12-07 17:18:36 -0500

    access by admin

--- seedapp/mbr/mbrApp.php
+++ seedapp/mbr/mbrApp.php
@@ -14,9 +14,9 @@ class MbrApp
         // the app that prints 3-up renewal and donation slips, and donation receipts
         'mbrPrint' =>
             [ 'renewalRequests'   => ['R MBR'],
-               'donationRequests' => ['R MBRDonations'],
-               'donationReceipts' => ['R MBRDonations'],
-               'donations'        => ['W MBRDonations'],
+               'donationRequests' => ['R MBRDonations', 'A MBR', '|'],
+               'donationReceipts' => ['R MBRDonations', 'A MBR', '|'],
+               'donations'        => ['W MBRDonations', 'A MBR', '|'],
                '|'  // allows screen-login even if some tabs are ghosted
             ],
     ];
