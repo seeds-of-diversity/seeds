a1ca84eebeb33898b4980caac22c3c24cff2b7fd
bob wildfong <bob@seeds.ca>
2021-01-19 12:06:13 -0500

    dev SEEDMail

--- seedlib/mail/SEEDMail.php
+++ seedlib/mail/SEEDMail.php
@@ -10,27 +10,34 @@
 include_once( "SEEDMailDB.php" );
 
 class SEEDMail
+/*************
+    Class for a SEEDMail record
+ */
 {
-    private $oApp;
+    public  $oApp;
     private $oDB;
+    private $kfr;
 
     function __construct( SEEDAppConsole $oApp, $k )
     {
         $this->oApp = $oApp;
         $this->oDB = new SEEDMailDB( $oApp );
+        $this->kfr = $k ? $this->oDB->GetKFR('M',$k) : $this->oDB->KFRel('M')->CreateRecord();
     }
 
-    function NewMail( $raParms )
-    /***************************
-        Create a new SEEDMail
+    function Key()  { return( $this->kfr->Key() ); }
+
+//    function GetKFR()  { return($this->kfr); }
+
+    function Store( $raParms )
+    /*************************
+        Copy the given parms to the current SEEDMail record and store it in the db
      */
     {
-
-    }
-
-    function UpdateMail( $kMail, $raParms )
-    {
-
+        foreach( $raParms as $k=>$v ) {
+            $this->kfr->SetValue( $k, $v );
+        }
+        $this->kfr->PutDBRow();
     }
 
     function StageMail( $kMail )
@@ -43,6 +50,37 @@ class SEEDMail
 
     }
 }
+
+
+class SEEDMailStaged
+/*******************
+    Class for a SEEDMail_Staged record
+ */
+{
+    private $oSMail;
+    private $oDB;
+    private $kfr;
+
+    function __construct( SEEDMail $oSMail )
+    {
+        $this->oSMail = $oSMail;
+        $this->oDB = new SEEDMailDB( $oApp );
+    }
+
+    function Store( $raParms )
+    /*************************
+        Copy the given parms into the SEEDMail_Staged record and store it in the db
+     */
+    {
+        $this->kfr = $this->oDB->KFRel('MS')->CreateRecord();
+        foreach( $raParms as $k=>$v ) {
+            $this->kfr->SetValue( $k, $v );
+        }
+        $this->kfr->SetValue('fk_SEEDMail', $this->oSMail->Key() );
+        $this->kfr->PutDBRow();
+    }
+}
+
 
 class SEEDMailSend
 {
