87a54dee71d27890723230bca557fe3acd6d4359
bobwildfong <bob@seeds.ca>
2021-06-29 09:56:06 -0400

    implement bCtrlSame

--- seedapp/sl/collection/batchopsTab_lotUpdates.php
+++ seedapp/sl/collection/batchopsTab_lotUpdates.php
@@ -13,6 +13,8 @@ class CollectionBatchOps_UpdateLots
     private $oSVA;  // where to keep state info for this tool
     private $oSLDB;
 
+    private $bCtrlSame = false;     // true if one set of controls applies to all Lots
+
     function __construct( SEEDAppConsole $oApp, SEEDSessionVarAccessor $oSVA )
     {
         $this->oApp = $oApp;
@@ -27,28 +29,28 @@ class CollectionBatchOps_UpdateLots
         // The left form contains rLots and defCtrl*, which define what is drawn on the right form.
         $oFormL = new SEEDCoreFormSVA( $this->oSVA, 'L', ['fields'=>['defCtrlLocation'=>['control'=>'checkbox'],
                                                                      'defCtrlGrams'   =>['control'=>'checkbox'],
-                                                                     'defCtrlDeAcc'   =>['control'=>'checkbox'] ]] );
+                                                                     'defCtrlDeAcc'   =>['control'=>'checkbox'],
+                                                                     'defCtrlSame'    =>['control'=>'checkbox']
+        ]] );
         $oFormL->Update();
-        $rLots = $oFormL->Value('rLots');
-        $bCtrlsSame = $oFormL->Value('bCtrlsSame');
+        $this->bCtrlSame = $oFormL->Value('defCtrlSame');
 
         // The right form contains controls for the defined Lots
         $oFormR = new SEEDCoreForm( 'R' );
         //$oFormR->Update();  processUpdates uses Deserialize to examine the response directly
 
         $sResults = $this->processUpdates( $oFormR );
-        $raLots = $this->getCurrentLots( $rLots );
 
         $s = "<div class='container-fluid'><div class='row'>"
 
             ."<div class='col-md-6'>"
                 ."<div style='margin:15px;padding:15px;border:1px solid #aaa'>"
-                ."<form>{$this->drawLeftForm($oFormL)}<br/><br/><input type='submit' value='Edit Lots'/></form>"
+                ."<form>{$this->drawLeftForm($oFormL, $oFormR)}<br/><input type='submit' value='Edit Lots'/></form>"
                 ."</div>"
             ."</div>"
             ."<div class='col-md-6'>"
                 .($sResults ? "<div style='background-color:#ddd;margin:10px;padding:10px;border-radius:5px'>$sResults</div>" : "")
-                ."<form>{$this->drawRightForm($oFormR,$oFormL,$raLots)}<br/><br/><input type='submit'></form>"
+                ."<form>{$this->drawRightForm($oFormL,$oFormR)}<br/><input type='submit'></form>"
             ."</div>"
 
             ."</div></div>";
@@ -61,17 +63,29 @@ class CollectionBatchOps_UpdateLots
         $sResults = "";
 
         $raRows = $oFormR->GetSEEDFormParms()->Deserialize( $_REQUEST )['rows'];
+        if( $this->bCtrlSame && count($raRows) ) {
+            // row 0 contains the values that apply to all Lots
+            $pLocation = $raRows[0]['values']['location'];
+            $pGrams    = $raRows[0]['values']['g_weight'];
+            $pDeAcc    = $raRows[0]['values']['bDeAcc'];
+        }
+
         foreach( $raRows as $ra ) {
             // $ra is the sf-encoded parms for each row of the form
             $kLot = $ra['values']['kLot'];
             if( ($kfr = $this->kfrLot($kLot)) ) {
-                if( ($p = $ra['values']['location']) && $p != $kfr->Value('location') ) {
+                $p = $this->bCtrlSame ? $pLocation : $ra['values']['location'];
+                if( $p && $p != $kfr->Value('location') ) {
                     $sResults .= "<p>Change {$kfr->Value('location')} to {$p} for Lot #$kLot</p>";
                 }
-                if( ($p = $ra['values']['g_weight']) && $p != $kfr->Value('g_weight') ) {
+
+                $p = $this->bCtrlSame ? $pGrams : $ra['values']['g_weight'];
+                if( $p && $p != $kfr->Value('g_weight') ) {
                     $sResults .= "<p>Change {$kfr->Value('g_weight')} to {$p} for Lot #$kLot</p>";
                 }
-                if( ($p = $ra['values']['bDeAcc']) && $p != $kfr->Value('bDeAcc') ) {
+
+                $p = $this->bCtrlSame ? $pDeAcc : $ra['values']['bDeAcc'];
+                if( $p && $p != $kfr->Value('bDeAcc') ) {
                     $sResults .= "<p>Change {$kfr->Value('bDeAcc')} to {$p} for Lot #$kLot</p>";
                 }
             }
@@ -99,23 +113,25 @@ class CollectionBatchOps_UpdateLots
         return( $this->oSLDB->GetKFRCond( 'IxAxPxS', "fk_sl_collection='1' AND inv_number='$kLot'") );
     }
 
-    private function drawLeftForm( SEEDCoreForm $oFormL )
+    private function drawLeftForm( SEEDCoreForm $oFormL, SEEDCoreForm $oFormR )
     {
         $s = "<div>Lot numbers:<br/>".$oFormL->Text('rLots')."</div>"
             ."<div>{$oFormL->Checkbox('defCtrlLocation')} Location</div>"
             ."<div>{$oFormL->Checkbox('defCtrlGrams')} Grams</div>"
-            ."<div>{$oFormL->Checkbox('defCtrlDeAcc')} Deaccession</div>";
+            ."<div>{$oFormL->Checkbox('defCtrlDeAcc')} Deaccession</div>"
+            ."<div style='margin-top:15px'>{$oFormL->Checkbox('defCtrlSame')} Same for every Lot</div>";
 
         return( $s );
     }
 
-    private function drawRightForm( SEEDCoreForm $oFormR, SEEDCoreForm $oFormL, $raLots )
+    private function drawRightForm( SEEDCoreForm $oFormL, SEEDCoreForm $oFormR )
     {
         $sR = "";
 
-        foreach( $raLots as $ra ) {
+        foreach( $this->getCurrentLots( $oFormL->Value('rLots') ) as $ra ) {
             $sCtrlLoc = $sCtrlGrams = $sCtrlDeAcc = "";
-            if( $ra['kfr'] ) {
+            if( !$this->bCtrlSame && $ra['kfr'] ) {
+                // make separate controls for each Lot
                 $sCtrlLoc .= $oFormL->Value('defCtrlLocation') ? $oFormR->Text('location') : "";
                 $sCtrlGrams .= $oFormL->Value('defCtrlGrams') ? $oFormR->Text('g_weight') : "";
                 $sCtrlDeAcc .= $oFormL->Value('defCtrlDeAcc') ? $oFormR->Text('bDeAcc') : "";
@@ -136,6 +152,16 @@ class CollectionBatchOps_UpdateLots
                 $sR .= $oFormR->Hidden( 'kLot', ['value'=>$ra['kLot']] );
                 $oFormR->IncRowNum();
             }
+        }
+
+        if( $this->bCtrlSame ) {
+            // make one set of controls that will apply to all Lots
+            $oFormR->SetRowNum(0);  // values will be found in sf-row 0
+            $sR .= "<div style='margin:10px;padding:10px;border:1px solid #aaa'>Apply Changes to All Lots"
+                  .($oFormL->Value('defCtrlLocation') ? "<div>{$oFormR->Text('location')} Location </div>" : "")
+                  .($oFormL->Value('defCtrlGrams') ? "<div>{$oFormR->Text('g_weight')} Grams</div>" : "")
+                  .($oFormL->Value('defCtrlDeAcc') ? "<div>{$oFormR->Text('bDeAcc')} Deaccessioned</div>" : "")
+                  ."</div>";
         }
 
         return( $sR );
