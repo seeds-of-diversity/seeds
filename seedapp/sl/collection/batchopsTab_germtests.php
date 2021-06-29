34f67f8366802620621a40d6899927bdab30119a
bobwildfong <bob@seeds.ca>
2021-06-29 07:58:40 -0400

    ui development

--- seedapp/sl/collection/batchopsTab_germtests.php
+++ seedapp/sl/collection/batchopsTab_germtests.php
@@ -1,3 +1,200 @@
 <?php
 
-// UI for the batch germ tests, which should use a common lib with the UI on the collection tab
+/* Seed collection manager - batch operations - record germination tests in batches
+ *
+ * Copyright 2020-2021 Seeds of Diversity Canada
+ */
+
+
+class Collection_GermTest
+{
+// factor common code from batchopsTab_germtest and collectionTab_germtest, and put this somewhere else
+}
+
+
+class CollectionBatchOps_GermTest
+{
+    private $oApp;
+    private $oSLDB;
+    private $sGermFeedback = "";
+
+    function __construct( SEEDAppConsole $oApp )
+    {
+        $this->oApp = $oApp;
+        $this->oSLDB = new SLDBCollection($this->oApp);
+    }
+
+    function Draw()
+    {
+        $s = "";
+
+        $sRowTmpl = "<tr>[[hiddenkey:]]
+                         <td style='width:10%'> @LotNum@ </td>
+                         <td style='width:15%'>[[XCvName     | width:100% | class='@XCvClass@' disabled]]</td>
+                         <td style='width:10%'>[[nSown       | width:100% | @OthersDisabled@ ]]</td>
+                         <td style='width:10%'>[[nGerm_count | width:100% | @OthersDisabled@ ]]</td>
+                         <td style='width:15%'>[[Date:dStart | width:100% | @OthersDisabled@ ]]</td>
+                         <td style='width:15%'>[[Date:dEnd   | width:100% | @OthersDisabled@ ]]</td>
+                         <td style='width:40%'>[[notes       | width:100% | @OthersDisabled@ ]]</td>
+                         <td style='width:40%'> @DelButton@</td>
+                     </tr>";
+
+        // the blank rows use a special lotnum field (onblur looks up the XCvName) and never have a delete button
+        $sRowBlank = str_replace( ['@LotNum@',                                    '@XCvClass@', '@OthersDisabled@', '@DelButton@'],
+                                  ["[[XLotNum | width:100% | class='gtLotNum']]", "gtCVName",   "",                 "&nbsp"],
+                                  $sRowTmpl );
+
+        // my rows have a normal disabled lotnum field, @DelButton@ is replaced per-row
+        $sRowMine = str_replace( ['@LotNum@',                                 '@XCvClass@', '@OthersDisabled@'],
+                                 ["[[I_inv_number| width:100% | disabled]]",  "",           ""],
+                                 $sRowTmpl );
+
+        // other peoples' rows are like mine but all disabled and with no delete button
+        $sRowOthers = str_replace( ['@LotNum@',                               '@XCvClass@', '@OthersDisabled@', '@DelButton@'],
+                                 ["[[I_inv_number| width:100% | disabled]]",  "",           "disabled",         ""],
+                                 $sRowTmpl );
+
+        // this is also in CollectionTab_GerminationTests
+        if( ($kDel = SEEDInput_Int('germdel')) && ($kfr = $this->oSLDB->GetKFR('G', $kDel)) ) {
+            $kfr->StatusSet( KeyframeRecord::STATUS_DELETED );
+            $kfr->PutDBRow();
+        }
+
+
+        $oForm = new KeyFrameForm( $this->oSLDB->KFRel('G'), 'A', ['DSParms'=>['fn_DSPreStore'=>[$this,'PreStoreGerm']] ] );
+        $oForm->Update();
+
+        // initialize form to draw blank entries
+        $oForm->SetKFR( $this->oSLDB->KFRel('G')->CreateRecord() );
+        // blank looks nicer than zero
+        $oForm->SetValue('nSown', '');
+        $oForm->SetValue('nGerm_count', '');
+
+        $oFE = new SEEDFormExpand( $oForm );
+        $s .= "<form method='post'><input type='submit'/>"
+             ."<table>"
+             ."<tr><th>Lot</th><th>Cultivar</th><th>Number Sown</th><th>Number Germ</th><th>Start Date</th><th>End Date</th><th>Notes</th></tr>";
+        for( $i = 0; $i < 5; ++$i ) {
+            $s .= $oFE->ExpandForm( $sRowBlank );
+            $oForm->IncRowNum();
+        }
+
+        //$s .= "<div id='collection-batch-germ-container'></div>";
+
+        $s .= "<tr><td colspan='7'>&nbsp;</td></tr>"
+             ."<tr><td colspan='7'>&nbsp;</td></tr>";
+
+        if( ($raKfrG = $this->oSLDB->KFRel('GxIxAxPxS')->GetRecordSet( 'dEnd is null', ['sSortCol'=>'dStart','bSortDown'=>true] )) ) {
+            $sMine = $sOthers = "";
+            foreach( $raKfrG as $kfr ) {
+                $oForm->SetKFR($kfr);
+
+                $oForm->SetValue( 'XLotNum', 'I_inv_number_already_set' );
+                $oForm->SetValue( 'XCvName', $oForm->Value('S_name_en').' '.$oForm->Value('P_name') );
+
+                // blank looks nicer than zero
+                if( !$oForm->Value('nGerm_count') ) $oForm->SetValue('nGerm_count', '');
+
+                if( $oForm->Value('_created_by')==$this->oApp->sess->GetUID() ) {
+                    $sMine .= $oFE->ExpandForm( str_replace( '@DelButton@', $this->deleteButton($oForm->GetKFR()), $sRowMine ) );
+                } else {
+                    $sOthers .= $oFE->ExpandForm( $sRowOthers );
+                }
+                $oForm->IncRowNum();
+            }
+            $s .= "<tr><td colspan='7'><h4>Your Tests In Progress</h4></td></tr>".$sMine;
+            if( $sOthers ) {
+                $s .= "<tr><td colspan='7'>&nbsp;</td></tr>"
+                     ."<tr><td colspan='7'><h4>Other Peoples' Tests In Progress</h4></td></tr>".$sOthers;
+            }
+        }
+
+        $s .= "</table></form>";
+
+        $s .= "<p style='margin-top:30px'>{$this->sGermFeedback}</p>";
+
+        $s .= "<p style='margin-top:30px;padding:10px;background-color:#ddd'>
+                 1. Enter Lot #, number of seeds sown.<br/>
+                 2. On first count enter number germinated.<br/>
+                 3. If further counts, update number germinated<br/>
+                 4. When all counts are done, set end date. Only records without end dates are shown here.
+               </p>";
+
+        return( $s );
+    }
+
+    // this is also in CollectionTab_GerminationTests
+    private function deleteButton( KeyframeRecord $kfr )
+    /***************************************************
+        Make a button that will delete the given germ test (only if it's your test)
+
+        sf{cid}d{R} doesn't work the way we want it to here, so do it with a custom parameter instead
+     */
+    {
+        $sDeleteButton = ($kfr->Key() && $kfr->Value('_created_by')==$this->oApp->sess->GetUID())
+                                ? ("<a href='{$this->oApp->PathToSelf()}?germdel={$kfr->Key()}'>"
+                                  ."<img src='".SEEDW_URL."img/ctrl/delete01.png' height='20'/>"
+                                  ."</a>")
+                                : "";
+        return( $sDeleteButton );
+    }
+
+    function PreStoreGerm( KeyFrame_DataStore $oDS )
+    {
+        if( $oDS->Key() ) {
+            // The I_inv_num is there because the server filled it in on an existing germ record
+            $iLot = ($ra = $this->oSLDB->GetRecordVals( 'GxIxAxPxS', $oDS->Key() )) ? $ra['I_inv_number'] : 0;      // really only need GxI
+
+        } else {
+            // The I_inv_num/XLotNum was typed into a new germ record.
+            // Validate that it's an integer, then look up the Lot and set fk_sl_inventory
+
+            if( !($iLot = $oDS->ValueInt('XLotNum')) ) {
+                // somebody typed a non-integer?
+                $this->oApp->oC->AddErrMsg( "Not processing lot # {$oDS->Value('XLotNum')}<br/>" );
+                return( false );
+            }
+            if( !($kfrLot = $this->oSLDB->GetKFRCond('I', "inv_number='$iLot' AND fk_sl_collection='1'")) ) {
+                $this->oApp->oC->AddErrMsg( "Lot # $iLot not found<br/>" );
+                return( false );
+            }
+
+            $oDS->SetValue( 'fk_sl_inventory', $kfrLot->Key() );
+        }
+
+        $oDS->CastInt('nSown');
+        $oDS->CastInt('nGerm_count');
+
+        if( !$oDS->Value('nSown') ) {
+            $this->oApp->oC->AddErrMsg( "Not recording record for lot # $iLot : 0 seeds sown<br/>" );
+            return( false );
+        }
+
+        // if dStart not defined default to today
+        if( !$oDS->Value('dStart') ) {
+            //$this->oApp->oC->AddUserMsg( "Defaulting Lot # $iLot test to today's date<br/>" );
+            $this->sGermFeedback .= "Defaulting Lot # $iLot test to today's date<br/>";
+            $oDS->SetValue( 'dStart', date('Y-m-d') );
+        }
+        // if dEnd not defined yet it has to be NULL in the db because DATE doesn't allow ''
+        if( !$oDS->Value('dEnd') ) {
+            $oDS->SetNull('dEnd');
+        }
+
+// temp: nGerm is %, should be the count
+        $oDS->SetValue( 'nGerm', $this->germPercent($oDS->Value('nSown'), $oDS->Value('nGerm_count')) );
+
+        // if dEnd is set this record will not be shown in the list anymore
+        if( $oDS->Value('dEnd') ) {
+            $this->sGermFeedback .= "Saving Lot # $iLot, {$oDS->Value('dStart')} to {$oDS->Value('dEnd')}, "
+                  ."germ {$oDS->ValueInt('nGerm_count')}/{$oDS->ValueInt('nSown')} ({$oDS->ValueInt('nGerm')}%) {$oDS->Value('notes')}<br/>";
+        }
+
+        return( true );
+    }
+
+    private function germPercent( $nSown, $nGerm_count )
+    {
+        return( $nSown ? intval(floatval($nGerm_count) / floatval($nSown) * 100.00) : 0 );
+    }
+}
--- seedapp/sl/collection/batchopsTab_lotUpdates.php
+++ seedapp/sl/collection/batchopsTab_lotUpdates.php
@@ -1,1 +1,117 @@
 <?php
+
+/* Seed collection manager - batch operations - update Lot information in batches
+ *
+ * Copyright 2021 Seeds of Diversity Canada
+ */
+
+include_once( SEEDCORE."SEEDCoreFormSession.php" );
+
+class CollectionBatchOps_UpdateLots
+{
+    private $oApp;
+    private $oSVA;  // where to keep state info for this tool
+    private $oSLDB;
+
+    function __construct( SEEDAppConsole $oApp, SEEDSessionVarAccessor $oSVA )
+    {
+        $this->oApp = $oApp;
+        $this->oSVA = $oSVA;
+        $this->oSLDB = new SLDBCollection($this->oApp);
+    }
+
+    function Draw()
+    {
+        $s = "";
+
+        // The left form contains rLots and defCtrl*, which define what is drawn on the right form.
+        $oFormL = new SEEDCoreFormSVA( $this->oSVA, 'L' );
+        $oFormL->Update();
+        $rLots = $oFormL->Value('rLots');
+        $bCtrlsSame = $oFormL->Value('bCtrlsSame');
+
+        // The right form contains controls for the defined Lots
+        $oFormR = new SEEDCoreForm( 'R' );
+        //$oFormR->Update();  processUpdates uses Deserialize to examine the response directly
+
+        $sResults = $this->processUpdates( $oFormR );
+        $raLots = $this->getCurrentLots( $rLots );
+
+        $s = "<div class='container-fluid'><div class='row'>"
+
+            ."<div class='col-md-6'>"
+                ."<div style='margin:15px;padding:15px;border:1px solid #aaa'>"
+                ."<form>{$this->drawLeftForm($oFormL)}<br/><br/><input type='submit' value='Use These Lot Numbers'/></form>"
+                ."</div>"
+            ."</div>"
+            ."<div class='col-md-6'>"
+                .($sResults ? "<div style='background-color:#ddd;margin:10px;padding:10px;border-radius:5px'>$sResults</div>" : "")
+                ."<form>{$this->drawRightForm($oFormR,$raLots)}<br/><br/><input type='submit'></form>"
+            ."</div>"
+
+            ."</div></div>";
+
+        return( $s );
+    }
+
+    private function processUpdates( SEEDCoreForm $oFormR )
+    {
+        $sResults = "";
+
+        $raRows = $oFormR->GetSEEDFormParms()->Deserialize( $_REQUEST )['rows'];
+        foreach( $raRows as $ra ) {
+            // $ra is the sf-encoded parms for each row of the form
+            $kLot = $ra['values']['kLot'];
+            if( ($kfr = $this->kfrLot($kLot)) ) {
+                if( $kfr->Value('P_name') != $ra['values']['cv'] ) {
+                    $sResults .= "<p>Change {$kfr->Value('P_name')} to {$ra['values']['cv']} for Lot #$kLot</p>";
+                }
+            }
+        }
+
+        return( $sResults );
+    }
+
+    private function getCurrentLots( $rLots )
+    {
+        $raLots = [];
+        foreach( SEEDCore_ParseRangeStrToRA($rLots) as $kLot ) {
+            $raLots[$kLot] = ['kLot'=>$kLot, 'cv'=>'Unknown Cultivar', 'bUnknown'=>true];
+
+            if( ($kfr = $this->kfrLot($kLot)) ) {
+                $raLots[$kLot]['cv'] = $kfr->Value('P_name');
+                $raLots[$kLot]['bUnknown'] = false;
+            }
+        }
+
+        return( $raLots );
+    }
+
+    private function kfrLot( $kLot )
+    {
+        return( $this->oSLDB->GetKFRCond( 'IxAxPxS', "fk_sl_collection='1' AND inv_number='$kLot'") );
+    }
+    private function drawLeftForm( SEEDCoreForm $oFormL )
+    {
+        $s = $oFormL->Text('rLots');
+
+        return( $s );
+    }
+
+    private function drawRightForm( SEEDCoreForm $oFormR, $raLots )
+    {
+        $sR = "";
+        foreach( $raLots as $ra ) {
+            $oFormR->SetValue('cv', $ra['cv']);
+            $sR .= "<div style='margin-bottom:10px;padding:10px;background-color:#ddd'>Lot {$ra['kLot']}: <em>{$ra['cv']}</em>";
+            if( !$ra['bUnknown'] ) {
+                $sR .= "<br/><br/>".$oFormR->Text('cv')
+                      .$oFormR->Hidden( 'kLot', ['value'=>$ra['kLot']] );
+                $oFormR->IncRowNum();
+            }
+            $sR .= "</div>";
+        }
+
+        return( $sR );
+    }
+}
