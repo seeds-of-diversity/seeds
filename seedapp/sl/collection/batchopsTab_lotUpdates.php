685702c8f177fd63f6bff7d5a77ad32356753983
bobwildfong <bob@seeds.ca>
2021-06-29 09:18:42 -0400

    controls can be chosen

--- seedapp/sl/collection/batchopsTab_lotUpdates.php
+++ seedapp/sl/collection/batchopsTab_lotUpdates.php
@@ -25,7 +25,9 @@ class CollectionBatchOps_UpdateLots
         $s = "";
 
         // The left form contains rLots and defCtrl*, which define what is drawn on the right form.
-        $oFormL = new SEEDCoreFormSVA( $this->oSVA, 'L' );
+        $oFormL = new SEEDCoreFormSVA( $this->oSVA, 'L', ['fields'=>['defCtrlLocation'=>['control'=>'checkbox'],
+                                                                     'defCtrlGrams'   =>['control'=>'checkbox'],
+                                                                     'defCtrlDeAcc'   =>['control'=>'checkbox'] ]] );
         $oFormL->Update();
         $rLots = $oFormL->Value('rLots');
         $bCtrlsSame = $oFormL->Value('bCtrlsSame');
@@ -41,12 +43,12 @@ class CollectionBatchOps_UpdateLots
 
             ."<div class='col-md-6'>"
                 ."<div style='margin:15px;padding:15px;border:1px solid #aaa'>"
-                ."<form>{$this->drawLeftForm($oFormL)}<br/><br/><input type='submit' value='Use These Lot Numbers'/></form>"
+                ."<form>{$this->drawLeftForm($oFormL)}<br/><br/><input type='submit' value='Edit Lots'/></form>"
                 ."</div>"
             ."</div>"
             ."<div class='col-md-6'>"
                 .($sResults ? "<div style='background-color:#ddd;margin:10px;padding:10px;border-radius:5px'>$sResults</div>" : "")
-                ."<form>{$this->drawRightForm($oFormR,$raLots)}<br/><br/><input type='submit'></form>"
+                ."<form>{$this->drawRightForm($oFormR,$oFormL,$raLots)}<br/><br/><input type='submit'></form>"
             ."</div>"
 
             ."</div></div>";
@@ -63,8 +65,8 @@ class CollectionBatchOps_UpdateLots
             // $ra is the sf-encoded parms for each row of the form
             $kLot = $ra['values']['kLot'];
             if( ($kfr = $this->kfrLot($kLot)) ) {
-                if( $kfr->Value('P_name') != $ra['values']['cv'] ) {
-                    $sResults .= "<p>Change {$kfr->Value('P_name')} to {$ra['values']['cv']} for Lot #$kLot</p>";
+                if( $ra['values']['location'] && $kfr->Value('location') != $ra['values']['location'] ) {
+                    $sResults .= "<p>Change {$kfr->Value('location')} to {$ra['values']['location']} for Lot #$kLot</p>";
                 }
             }
         }
@@ -73,15 +75,14 @@ class CollectionBatchOps_UpdateLots
     }
 
     private function getCurrentLots( $rLots )
+    /****************************************
+        array of [kLot, kfr, cv] and/or [kLot, null, 'Unknown Cultivar']
+     */
     {
         $raLots = [];
         foreach( SEEDCore_ParseRangeStrToRA($rLots) as $kLot ) {
-            $raLots[$kLot] = ['kLot'=>$kLot, 'cv'=>'Unknown Cultivar', 'bUnknown'=>true];
-
-            if( ($kfr = $this->kfrLot($kLot)) ) {
-                $raLots[$kLot]['cv'] = $kfr->Value('P_name');
-                $raLots[$kLot]['bUnknown'] = false;
-            }
+            $raLots[$kLot] = ['kLot'=>$kLot, 'kfr'=>$this->kfrLot($kLot)];
+            $raLots[$kLot]['cv'] = $raLots[$kLot]['kfr'] ? $raLots[$kLot]['kfr']->Value('P_name') : 'Unknown Cultivar';
         }
 
         return( $raLots );
@@ -91,25 +92,44 @@ class CollectionBatchOps_UpdateLots
     {
         return( $this->oSLDB->GetKFRCond( 'IxAxPxS', "fk_sl_collection='1' AND inv_number='$kLot'") );
     }
+
     private function drawLeftForm( SEEDCoreForm $oFormL )
     {
-        $s = $oFormL->Text('rLots');
+        $s = "<div>Lot numbers:<br/>".$oFormL->Text('rLots')."</div>"
+            ."<div>{$oFormL->Checkbox('defCtrlLocation')} Location</div>"
+            ."<div>{$oFormL->Checkbox('defCtrlGrams')} Grams</div>"
+            ."<div>{$oFormL->Checkbox('defCtrlDeAcc')} Deaccession</div>";
 
         return( $s );
     }
 
-    private function drawRightForm( SEEDCoreForm $oFormR, $raLots )
+    private function drawRightForm( SEEDCoreForm $oFormR, SEEDCoreForm $oFormL, $raLots )
     {
         $sR = "";
+
         foreach( $raLots as $ra ) {
-            $oFormR->SetValue('cv', $ra['cv']);
-            $sR .= "<div style='margin-bottom:10px;padding:10px;background-color:#ddd'>Lot {$ra['kLot']}: <em>{$ra['cv']}</em>";
-            if( !$ra['bUnknown'] ) {
-                $sR .= "<br/><br/>".$oFormR->Text('cv')
-                      .$oFormR->Hidden( 'kLot', ['value'=>$ra['kLot']] );
+            $sCtrlLoc = $sCtrlGrams = $sCtrlDeAcc = "";
+            if( $ra['kfr'] ) {
+                $sCtrlLoc .= $oFormL->Value('defCtrlLocation') ? $oFormR->Text('location') : "";
+                $sCtrlGrams .= $oFormL->Value('defCtrlGrams') ? $oFormR->Text('g_weight') : "";
+                $sCtrlDeAcc .= $oFormL->Value('defCtrlDeAcc') ? $oFormR->Text('bDeAcc') : "";
+            }
+
+            $sR .= "<div style='margin-bottom:10px;padding:10px;background-color:#ddd'>"
+                      ."<table border='0'>"
+                          ."<tr><td>Lot {$ra['kLot']}: <em>{$ra['cv']}</em></td><td>&nbsp</td></tr>"
+                          .($ra['kfr'] ?
+                              ("<tr><td>Location: {$ra['kfr']->Value('location')}</td><td>$sCtrlLoc</td></tr>"
+                              ."<tr><td>Grams: {$ra['kfr']->Value('g_weight')}</td><td>$sCtrlGrams</td></tr>"
+                              ."<tr><td>Deaccessioned: {$ra['kfr']->Value('bDeAcc')}</td><td>$sCtrlDeAcc</td></tr>")
+                              : "")
+                      ."</table>"
+                  ."</div>";
+
+            if( $ra['kfr'] ) {
+                $sR .= $oFormR->Hidden( 'kLot', ['value'=>$ra['kLot']] );
                 $oFormR->IncRowNum();
             }
-            $sR .= "</div>";
         }
 
         return( $sR );
