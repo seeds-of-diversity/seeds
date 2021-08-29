70f58faf6af3ec875f391ba8c6711f06fd257ae2
bobwildfong <bob@seeds.ca>
2021-08-28 21:11:46 -0400

    add sessions to WP plugin

--- seedapp/website/seedsWPPlugin.php
+++ seedapp/website/seedsWPPlugin.php
@@ -5,9 +5,24 @@ include_once( SEEDLIB."SEEDTemplate/masterTemplate.php" );
 include_once( SITEROOT."drupalmod/lib/d8_seedbreeze.php" );
 
 
+function seedsWPStart()
+{
+    // replace SEEDTemplate tags in content
+    add_filter( 'the_content', 'seedsWPPlugin_Filter' );
+
+    // stop WP default behaviour that e.g. turns '--' into &mdash; which breaks some image names in SEEDTags
+    remove_filter( 'the_content', 'wptexturize' );
+
+    // WP is stateless so it doesn't use sessions.
+    // We start_session() when a SEEDSession is created, but that doesn't seem to happen early enough so we also do it here.
+    add_action( 'init',      'seedsWPPlugin_SessionStart', 1 );
+    add_action( 'wp_logout', 'seedsWPPlugin_SessionEnd');    // only necessary if the user is logging in/out of WP and needs session wiped
+    add_action( 'wp_login',  'seedsWPPlugin_SessionEnd');    // only necessary if the user is logging in/out of WP and needs session wiped
+}
+
+
 function seedsWPPlugin_Filter( $content )
 {
-
     $oApp = SEEDConfig_NewAppConsole_LoginNotRequired( [] );
 
     $oTmpl = new Drupal8Template( $oApp, [] );
@@ -18,12 +33,16 @@ function seedsWPPlugin_Filter( $content )
 
 //    $content = SEEDROOT." ".SEEDW." ".SEEDW_URL
 //              ."<br/><br/>".$content;
-    
+
     return( $content );
 }
 
-function seedsWPStart()
-{
-    add_filter( 'the_content', 'seedsWPPlugin_Filter' );
-    remove_filter( 'the_content', 'wptexturize' );  // because WP turns '--' into &mdash; which breaks some image names in SEEDTags
+function seedsWPPlugin_SessionStart() {
+    if( !session_id() ) {
+        session_start();
+    }
+}
+
+function seedsWPPlugin_SessionStart() {
+    session_destroy ();
 }
