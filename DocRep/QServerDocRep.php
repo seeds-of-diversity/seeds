4d68d91a2dc1402b3a8bd2c1d27283eac16ae66e
Leo Zeng <leozeng1478@gmail.com>
2022-01-28 14:35:26 -0500

    added css for formatting the diff view
    removed unnessessary config code for diff view library

--- DocRep/QServerDocRep.php
+++ DocRep/QServerDocRep.php
@@ -199,6 +199,7 @@ class QServerDocRep extends SEEDQ
             $current = $oDoc1->GetValuesVer($ver1)['data_text'];
             $previous = $oDoc2->GetValuesVer($ver2)['data_text'];
             
+            // add new line so php-diff will display it as multiple lines 
             $current = str_replace("</p>", "</p>\n", $current);
             $current = str_replace("</h1>", "</h1>\n", $current);
             $current = str_replace("</h2>", "</h2>\n", $current);
@@ -214,10 +215,9 @@ class QServerDocRep extends SEEDQ
             // TODO: add something like if a paragraph is multiple lines, put a \n every 30 char 
             
             
-            /*
-             * php diff config information 
-             * TODO: remove everything except for the needed stuff 
-             */
+            // config for php-diff 
+            // list of configs available can be found at 
+            // https://github.com/jfcherng/php-diff
             
             // renderer class name:
             //     Text renderers: Context, JsonText, Unified
@@ -226,75 +226,17 @@ class QServerDocRep extends SEEDQ
             
             // the Diff class options
             $differOptions = [
-                // show how many neighbor lines
-                // Differ::CONTEXT_ALL can be used to show the whole file
-                'context' => 3,
-                // ignore case difference
-                'ignoreCase' => false,
-                // ignore whitespace difference
-                'ignoreWhitespace' => false,
+                
             ];
             
             // the renderer class options
             $rendererOptions = [
-                // how detailed the rendered HTML in-line diff is? (none, line, word, char)
-                'detailLevel' => 'line',
-                // renderer language: eng, cht, chs, jpn, ...
-                // or an array which has the same keys with a language file
-                'language' => 'eng',
-                // show line numbers in HTML renderers
-                'lineNumbers' => true,
-                // show a separator between different diff hunks in HTML renderers
-                'separateBlock' => true,
-                // show the (table) header
-                'showHeader' => true,
-                // the frontend HTML could use CSS "white-space: pre;" to visualize consecutive whitespaces
-                // but if you want to visualize them in the backend with "&nbsp;", you can set this to true
-                'spacesToNbsp' => false,
-                // HTML renderer tab width (negative = do not convert into spaces)
-                'tabSize' => 4,
-                // this option is currently only for the Combined renderer.
-                // it determines whether a replace-type block should be merged or not
-                // depending on the content changed ratio, which values between 0 and 1.
-                'mergeThreshold' => 0.8,
-                // this option is currently only for the Unified and the Context renderers.
-                // RendererConstant::CLI_COLOR_AUTO = colorize the output if possible (default)
-                // RendererConstant::CLI_COLOR_ENABLE = force to colorize the output
-                // RendererConstant::CLI_COLOR_DISABLE = force not to colorize the output
-                'cliColorization' => RendererConstant::CLI_COLOR_AUTO,
-                // this option is currently only for the Json renderer.
-                // internally, ops (tags) are all int type but this is not good for human reading.
-                // set this to "true" to convert them into string form before outputting.
-                'outputTagAsString' => false,
-                // this option is currently only for the Json renderer.
-                // it controls how the output JSON is formatted.
-                // see available options on https://www.php.net/manual/en/function.json-encode.php
-                'jsonEncodeFlags' => \JSON_UNESCAPED_SLASHES | \JSON_UNESCAPED_UNICODE,
-                // this option is currently effective when the "detailLevel" is "word"
-                // characters listed in this array can be used to make diff segments into a whole
-                // for example, making "<del>good</del>-<del>looking</del>" into "<del>good-looking</del>"
-                // this should bring better readability but set this to empty array if you do not want it
-                'wordGlues' => [' ', '-'],
-                // change this value to a string as the returned diff if the two input strings are identical
-                'resultForIdenticals' => null,
-                // extra HTML classes added to the DOM of the diff container
-                'wrapperClasses' => ['diff-wrapper'],
+                'detailLevel' => 'word',
             ];
-            
-            //$currentFile = SEEDROOT.'current.txt';
-            //$previousFile = SEEDROOT.'previous.txt';
-            
-            //file_put_contents($currentFile, $current);
-            //file_put_contents($previousFile, $previous);
-            
-            
-
-            //$differ = new Differ(explode("\n", $previous), explode("\n", $current), $differOptions);
-            //$renderer = RendererFactory::make($rendererName, $rendererOptions); // or your own renderer object
-            //$result = $renderer->render($differ);
             
             $result = DiffHelper::calculate($previous, $current, $rendererName, $differOptions, $rendererOptions);
             
+            // show html elements as formatting
             $result = str_replace("&lt;", "<", $result);
             $result = str_replace("&gt;", ">", $result);
             $result = str_replace("&amp;", "&", $result);
