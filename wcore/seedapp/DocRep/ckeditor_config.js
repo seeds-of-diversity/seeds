41159c881dce05c431480fb6face2a5068e5c66c
Leo Zeng <leozeng1478@gmail.com>
2022-01-31 17:21:26 -0500

    ckeditor config file

--- wcore/seedapp/DocRep/ckeditor_config.js
+++ wcore/seedapp/DocRep/ckeditor_config.js
@@ -1,2 +1,24 @@
 // edit this file to change config for ckeditor
+// this is the default config
 console.log('ckeditor config file');
+
+/*
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
+};
+*/
