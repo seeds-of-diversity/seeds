f7160a6d92364cb6d632fd6ad686ed8a5c5329b9
bobwildfong <bob@seeds.ca>
2022-03-13 17:08:48 -0400

    start of app

--- seedapp/events/app_eventmanager.php
+++ seedapp/events/app_eventmanager.php
@@ -2,7 +2,7 @@
 
 /* app_eventmanager.php
  *
- * Copyright 2010-2021 Seeds of Diversity Canada
+ * Copyright 2010-2022 Seeds of Diversity Canada
  *
  * Manage event lists
  */
@@ -22,28 +22,83 @@ if( !defined( "SEEDROOT" ) ) {
 
 include("../../seedlib/google/GoogleSheets.php" );
 
-$oApp = SEEDConfig_NewAppConsole( ['db'=>'seeds2', 'sessPermsRequired'=>["W events"]] );
+$oApp = SEEDConfig_NewAppConsole_LoginNotRequired( ['db'=>'seeds2'] ); //, 'sessPermsRequired'=>["W events"]] );
 
 
+$oES = new EventsSheet($oApp);
 
-$sheets = new SEEDGoogleSheets( ['appName' => 'My PHP App',
-                                 'authConfigFname' => SEEDCONFIG_DIR."/sod-public-outreach-info-e36071bac3b1.json",
-                                 'idSheet' => "1npC38lFv84iG1YhfS9oBpiLXYjbrjiXqHQTaibonl5I" //"1NkwvvyO71XcRlsGK9vEi2T2uB_b0fJ5-LdwFcCAeDQk"
-                                ] );
+if( $oES->IsLoaded() ) {
 
-$spreadsheetId = "1l1WuajTKtVqLTTR7vWCoDan-vD9WUS9li7ErA5BXThI";
-$spreadsheetId = "";
-$range = 'A2:H';
-list($values,$range) = $sheets->GetValues( $range );
-
-$sheets->WriteValues( 'Sheet1!A23:B23', [['1','2']] );
-
-var_dump($values);
+    /* Try to match up the columns
+     */
 
 
 
 
 
+    //$sheets->WriteValues( 'Sheet1!A23:B23', [['1','2']] );
+
+    //var_dump($oES->values());
+}
+
+$s = $oES->DrawForm()
+    .$oES->DrawTable();
+
+echo $s;
 
 
+class EventsSheet
+/****************
+    Connect events with a Google sheet
+ */
+{
+    private $oApp;
 
+    private $oSheets = null;
+    private $p_idSheet;
+
+    function __construct( SEEDAppConsole $oApp )
+    {
+        $this->oApp = $oApp;
+
+        $this->p_idSheet = SEEDInput_Str('idSheet');
+
+        if( $this->p_idSheet ) {
+            $this->oSheets = new SEEDGoogleSheets(
+                                    ['appName' => 'My PHP App',
+                                     'authConfigFname' => SEEDCONFIG_DIR."/sod-public-outreach-info-e36071bac3b1.json",
+                                     'idSpreadsheet' => $this->p_idSheet,
+                                     //'idSheet' => "1npC38lFv84iG1YhfS9oBpiLXYjbrjiXqHQTaibonl5I"
+                                     //"1NkwvvyO71XcRlsGK9vEi2T2uB_b0fJ5-LdwFcCAeDQk"
+                                     //"1l1WuajTKtVqLTTR7vWCoDan-vD9WUS9li7ErA5BXThI"
+                                    ] );
+        }
+    }
+
+    function IsLoaded()  { return( $this->oSheets ); }
+
+    function values()
+    {
+        $range = 'A1:Z1';
+        list($values,$range) = $this->oSheets->GetValues( $range );
+        return( $values );
+    }
+
+    function DrawForm()
+    {
+        $s = "<form method='post'>
+              <div><input type='text' name='idSheet' value='{$this->p_idSheet}' size='60'/>&nbsp;
+                   <input type='submit'/>
+              </div>
+              </form>";
+
+        return( $s );
+    }
+
+    function DrawTable()
+    {
+        $s = "";
+
+        return( $s );
+    }
+}
