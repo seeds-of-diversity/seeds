f4acbbd38955a3702aa9766e1fab62408f05a4b3
bobwildfong <bob@seeds.ca>
2022-11-10 16:31:50 -0500

    updates to events manager

--- seedlib/events/events.php
+++ seedlib/events/events.php
@@ -1,16 +1,77 @@
 <?php
 
+/* events.php
+ *
+ * Copyright (c) 2019-2022 Seeds of Diversity Canada
+ *
+ * Record and manage events with volunteers and exhibit materials
+ *
+ * type:
+ *  SS: title is not stored: created via "Seedy Saturday/Sunday {city}"
+ *      city = the city or town
+ *      location = the venue and address
+ *
+ *  EV: title = the name of the event
+ *      city = the city or town
+ *      location = the venue and address
+ *
+ *  VIRTUAL:
+ *      title = the name of the event
+ *      city = blank
+ *      province = blank
+ *      location = blank
+ */
+
 include_once( "eventsDB.php" );
 
 class EventsLib
 {
     public $oApp;
     public $oDB;
+    private $oTag;
 
     function __construct( SEEDAppSessionAccount $oApp )
     {
         $this->oApp = $oApp;
         $this->oDB = new EventsDB( $oApp );
+
+        /* Certain fields can contain SEEDTags. Do local ResolveTag first then BasicResolver
+         */
+        $this->oBasicResolver = new SEEDTagBasicResolver( ['bLinkTargetBlank'=>true,             // force links to open new windows/tabs
+                                                           'bLinkPlainDefaultCaption'=>true] );  // use a nice default caption for the link
+        $raResolvers = [ ['fn'=>[$this,'ResolveTag'], 'raParms'=>[]],
+                         ['fn'=>[$this->oBasicResolver,'ResolveTag'], 'raParms'=>[]]
+                       ];
+        $this->oTag = new SEEDTagParser( ['raResolvers' => $raResolvers] );
+    }
+
+    function ExpandStr( $s )
+    {
+        return( $this->oTag->ProcessTags($s) );
+    }
+
+    function ResolveTag( $raTag, SEEDTagParser $oTagDummy, $raParmsDummy )
+    /*********************************************************************
+        Called before SEEDTag's BasicResolver. Does these conversions:
+            1) tags with no namespace that look like email addresses [[foo@seeds.ca]]          -> mailto:foo@seeds.ca
+            2) tags with no namespace that don't look like email addresses [[seeds.ca/events]] -> http://seeds.ca/events
+
+            N.B. This actually changes [[seeds.ca/events]] to [[http:seeds.ca/events]] without the forward slashes.
+                 BasicResolver knows to do the right thing.
+     */
+    {
+        $bHandled = false;
+        $bRetagged = false;
+        $s = "";
+
+        if( $raTag['tag'] == '' ) {
+            // If the tag has no namespace change it to http or mailto.
+            $raTag['tag'] = (strpos($raTag['target'],'@') !== false) ? 'mailto' : 'http';
+            $bRetagged = true;
+        }
+
+        // ReTagging works by returning true as the third return value and a new raTag as the fourth. Subsequent Resolvers will use the new raTag.
+        return( [$bHandled, $s, $bRetagged, $raTag] );
     }
 }
 
@@ -72,15 +133,9 @@ class Events_event
         $city     = $this->kfr->Expand( "[[city]], [[province]]" );
         $location = $this->kfr->ValueEnt("location");
         $title    = $this->GetTitle( $this->kfr );
+        $date     = $this->_getValue( "date_alt" ) ?: SEEDDateDB2Str( $this->kfr->value("date_start"), $this->oE->oApp->lang );
 
-        $date = $this->_getValue( "date_alt" ) ?: SEEDDateDB2Str( $this->kfr->value("date_start"), $this->oE->oApp->lang );
-
-        $s = $city;
-        return( $s );
-
-
-
-        switch( $kfrEV->value("type") ) {
+        switch( $this->kfr->value("type") ) {
             case 'EV':
             default:
                 // show title, city, province, location as recorded
@@ -96,61 +151,59 @@ class Events_event
                 $location = "";
         }
 
+        $s = "<div class='EV_Event'>".SEEDCore_ArrayExpandSeries($this->kfr->ValuesRA(), "[[k]]=>[[v]]<br/>").$this->kfr->Key()
+                ."<h3>$title</h3>"
+                ."<p><strong>"
+                    .$date.SEEDCore_NBSP("",6).$this->kfr->value("time")."<br/>"
+                    .($location ? "$location<br/>" : "")
+                    .($city     ? "$city<br/>"     : "")
+                ."</b></p>"
+                .$this->drawEventDetails()
+                .(($c = $this->kfr->Value("contact")) ? "<p>Contact: {$this->oE->ExpandStr($c)}</p>" : "")
+                .(($u = $this->kfr->Value("url_more"))
+                        ? ("<p>$u".($this->oE->oApp->lang == 'FR' ? "Plus d'information" : "More information").": {$this->oE->ExpandStr($u)}</p>")
+                        : "" )
+                .(($x = $this->kfr->Value("url_more"))
+                        ? ("<p>AAA<a style='text-decoration:none;' target='_blank' href='$x'>BBB".$x
+                     ."<div class='btn btn-success'>"
+                     .($this->oE->oApp->lang == 'FR' ? "Plus d'information" : "More information")
+                     ."</div>"
+                     ."</a>CCC</p>"
+                        )
+                        : "")
+                ."</div>";
 
-
-        if( $this->bPrn ) {
-            // only used in unilingual mode
-
-            // this is not the best format for EV-type events, but it works for SS
-
-            $s .= "<DIV class='evPRNTitle'>$title</DIV>";
-            $s .= "</TD><TD valign=top>";
-            $s .= "<P><B>"
-                 .$date . SEEDStd_StrNBSP("",6) . $kfrEV->value("time")."</B>"
-                 .( !empty($location) ? ("<BR>".$location) : "")
-                 ."</P>";
-            $s .= $this->_drawEventDetails( $kfrEV );
-            if( $kfrEV->IsEmpty("contact") ) {
-                $s .= "<P>Contact: ".$kfrEV->valueEnt("contact")."</P>";
-            }
-            $s .= "</TD></TR>\n<TR><TD align=left valign=top>";
-        } else {
-            $s = "<DIV class='EV_Event'>";
-
-            $s .= "<H3>$title</H3>"
-                 //."<BLOCKQUOTE>"
-                 ."<P><B>"
-                 .$date .SEEDStd_StrNBSP("",10).$kfrEV->value("time")."<br/>"
-                 .(!empty($location) ? ($location."<br/>") : "")
-                 .(!empty($city) ? ($city."<br/>") : "")
-                 ."</B></P>";
-            $s .= $this->_drawEventDetails( $kfrEV );
-            if( !$kfrEV->IsEmpty("contact") ) {
-                $s .= "<P>Contact: "
-                     .($this->oWiki ? $this->oWiki->TranslateLinksOnly($kfrEV->value("contact")) : $kfrEV->value("contact"))
-                     ."</P>";
-            }
-            if( !$kfrEV->IsEmpty("url_more") ) {
-                $s .= "<p>".($this->lang == 'FR' ? "Plus d'information" : "More information").": "
-                     .$this->oTag->ProcessTags( $kfrEV->value("url_more") )
-                     ."</p>";
 /*
  A new More Information link
 
                 $sUrl = $kfrEV->value("url_more");
-                $s .= "<a style='text-decoration:none;' target='_blank' href='$sUrl'>"
-                     ."<div class='btn btn-success'>"
-                     .($this->lang == 'FR' ? "Plus d'information" : "More information")
-                     ."</div>"
-                     ."</a>";
+                $s .= "";
 */
-            }
-            $s .= //"</BLOCKQUOTE>"
-            "</DIV>\n";
-        }
 
-
+            return( $s );
     }
+
+    private function drawEventDetails()
+    /**********************************
+     */
+    {
+        $details = $this->_getValue( "details", false );    // do not expand entities because this is allowed to contain HTML
+        $details = trim($details);                                  // get rid of trailing blank lines
+
+        if( intval(substr($this->kfr->value("date_start"),0,4)) < 2008 ) {
+            // prior to 2008 we used plaintext, now use Wiki
+            $s = nl2br($details);
+        } else {
+            $details = nl2br($details);
+//correct new way            $s = $this->oTag->ProcessTags( $details );
+
+//REMOVE
+      //      $s = ($this->oWiki ? $this->oWiki->TranslateLinksOnly($details) : $details);
+            $s = $details;
+        }
+        return( "<p style='width:80%'>$s</p>" );
+    }
+
 
     function GetVolunteerLine()
     {
--- seedlib/events/eventsDB.php
+++ seedlib/events/eventsDB.php
@@ -2,8 +2,11 @@
 
 class EventsDB extends Keyframe_NamedRelations
 {
+    private $oApp;
+
     function __construct( SEEDAppSessionAccount $oApp )
     {
+        $this->oApp = $oApp;
         parent::__construct( $oApp->kfdb, $oApp->sess->GetUID(), $oApp->logdir );
     }
 
@@ -12,15 +15,53 @@ class EventsDB extends Keyframe_NamedRelations
         $raKfrel = array();
 
         $def = ["Tables" => [
-                    "E" => ["Table" => "seeds_1.ev_events",
+                    "E" => ["Table" => "{$this->oApp->DBName('seeds1')}.ev_events",
                             "Type"  => 'Base',
                             "Fields" => 'Auto'
                ]]];
 
         $parms = $logdir ? ['logfile'=>$logdir."events.log"] : [];
 
-        $raKfrel['E']   = new Keyframe_Relation( $kfdb, $def, $uid, $parms );
+        $raKfrel['E'] = new Keyframe_Relation( $kfdb, $def, $uid, $parms );
 
         return( $raKfrel );
     }
+
+    const SqlCreate = "
+        CREATE TABLE IF NOT EXISTS ev_events (
+                _key        INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                _created    DATETIME,
+                _created_by INTEGER,
+                _updated    DATETIME,
+                _updated_by INTEGER,
+                _status     INTEGER DEFAULT 0,
+
+            type        enum('SS','EV','VIRTUAL') NOT NULL DEFAULT 'SS',  # SS = Seedy Sat/Sun, EV = regular, VIRTUAL = no location
+            date_start  DATE NOT NULL DEFAULT '2017-01-01',
+            date_end    DATE NOT NULL DEFAULT '2017-01-01',
+            date_alt    VARCHAR(200),
+            date_alt_fr VARCHAR(200),
+            time        VARCHAR(200),
+            location    VARCHAR(200),
+            city        VARCHAR(200),
+            province    VARCHAR(200),
+            title       VARCHAR(200),
+            title_fr    VARCHAR(200),
+            spec        VARCHAR(200),                           # control tags (like texttype for the details)
+            details     TEXT,
+            details_fr  TEXT,
+            contact     VARCHAR(200),
+            url_more    VARCHAR(200),                           # click for more info, poster, special page, etc
+            latlong     VARCHAR(200),                           # latitude and longitude urlencoded (blank means it needs to be geocoded)
+            attendance  INTEGER,
+            notes_priv  TEXT,                                   # internal notes
+
+            vol_kMbr    INTEGER NOT NULL DEFAULT 0,             # our main volunteer there
+            vol_notes   TEXT,                                   # materials to send and notes about volunteer/event
+            vol_dSent   VARCHAR(20),                            # YYYY-MM-DD when materials sent ('' == not sent yet, anything else means no need to send)
+
+            INDEX (date_start),
+            INDEX (province)
+        );
+    ";
 }
