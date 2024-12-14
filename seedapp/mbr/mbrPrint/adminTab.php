dc9271607b936a408135cbfb553636b10c9b723b
bobwildfong <bob@seeds.ca>
2024-12-14 11:50:03 -0500

    many improvements

--- seedapp/mbr/mbrPrint/adminTab.php
+++ seedapp/mbr/mbrPrint/adminTab.php
@@ -5,19 +5,17 @@ include_once( SEEDCORE."console/console02ui.php");
 class MbrDonationsTab_Admin
 {
     private $oApp;
-//    private $oSVA;  // session vars for the Console tab
+    private $oSVA;  // session vars for the Console tab
     private $oOpPicker;
-    private $oMbrDb;
 
     function __construct( SEEDAppConsole $oApp, SEEDSessionVarAccessor $oSVA )
     {
         $this->oApp = $oApp;
         $this->oSVA = $oSVA;    // this tab's SVA
-        $raOps = ['-- Choose --'=>'',
-                  'Integrity test'                 =>'integrity',
-                  'Upload Canada Helps spreadsheet'=>'uploadCH'];
-        $this->oOpPicker = new Console02UI_OperationPicker('currOp', $oSVA, $raOps);
-        $this->oMbrDb = new Mbr_ContactsDB($oApp);
+        $this->oOpPicker = new Console02UI_OperationPicker('currOp', $oSVA,
+                                ['-- Choose --'=>'',
+                                 'Integrity test'                 =>'integrity',
+                                 'Upload Canada Helps spreadsheet'=>'uploadCH'] );
     }
 
     function Init()
@@ -38,20 +36,37 @@ class MbrDonationsTab_Admin
                 break;
 
             case 'uploadCH':
-                $s = $this->doUpload();
+                $s = (new MbrDonationsTab_Admin_CHUpload($this->oApp, $this->oSVA))->Main();
                 break;
         }
 
         return( $s );
     }
+}
+
+class MbrDonationsTab_Admin_CHUpload
+{
+    private $oApp;
+    private $oSVA;
+    private $oMbr;
 
     private $raSheetCols =
-        ['trid'=>'TRANSACTION NUMBER',
-         'monthly'=>'MONTHLY GIFT ID',
-         'email'=>'DONOR EMAIL ADDRESS',
+        ['trid'     => 'TRANSACTION NUMBER',
+         'monthly'  => 'MONTHLY GIFT ID',
+         'email'    => 'DONOR EMAIL ADDRESS',
+         'don_date' => 'DONATION DATE',
+         'amount'   => 'AMOUNT',
+         'lastname' => 'DONOR LAST NAME'
         ];
 
-    private function doUpload()
+    function __construct( SEEDAppConsole $oApp, SEEDSessionVarAccessor $oSVA )
+    {
+        $this->oApp = $oApp;
+        $this->oSVA = $oSVA;
+        $this->oMbr = new Mbr_Contacts($oApp);
+    }
+
+    function Main()
     {
         $s = "";
 
@@ -76,8 +91,13 @@ class MbrDonationsTab_Admin
                 $this->uploadPreProcess($raRawRows);
                 // Show the differences between the uploaded data and the db data
                 $s .= "<h3>Uploaded ".count($raRawRows)." rows</h3>"
-                     .$this->uploadDrawValidateView();
-                break;
+                     .$this->uploadDrawDiff();
+                // draw Commit button
+                $s .= "<form action='{$this->oApp->PathToSelf()}'>
+                       <input type='submit' value='Commit Changes'/>
+                       {$oForm->Hidden('cmd', ['value'=>'uploadCommit'])}
+                       </form>";
+                    break;
 
             case 'uploadCommit':
                 // commit the changes based on processed data
@@ -106,22 +126,6 @@ class MbrDonationsTab_Admin
         return($s);
     }
 
-    private function uploadDrawValidateView()
-    {
-        $s = "";
-
-        $s .= $this->uploadDrawDiff();
-
-        // draw Commit button
-        $oForm = new SEEDCoreForm('A');
-        $s .= "<form action='{$this->oApp->PathToSelf()}'>
-               <input type='submit' value='Commit Changes'/>
-               {$oForm->Hidden('cmd', ['value'=>'uploadCommit'])}
-               </form>";
-
-        return($s);
-    }
-
     private function uploadDrawDiff()
     {
         $s = "";
@@ -137,6 +141,11 @@ class MbrDonationsTab_Admin
         foreach($raCHData['raSingle'] as $k => $raD) {
             $s .= $this->uploadDrawDiffRecord($k, $raD);
         }
+        $s .= "<tr><td></td></tr>
+               <tr><th>Ignored donations</th><th>&nbsp;</th><th>&nbsp;</th><th></th></tr>";
+        foreach($raCHData['raIgnored'] as $k => $raD) {
+            $s .= "<tr><td>$k : {$raD['name']} {$raD['amount']} on {$raD['date_db']}</td></tr>";
+        }
 
         $s .= "</table>";
 
@@ -147,7 +156,7 @@ class MbrDonationsTab_Admin
     {
         $s = "";
 
-        [$status,$errmsg] = array_pad(explode(':', $raD['status']), 2, "");
+        [$status,$errmsg] = SEEDCore_Explode(':', $raD['status'], 2);
 
         $s1 = "Found $k";
         $s2 = $raD['M_name'];
@@ -156,28 +165,30 @@ class MbrDonationsTab_Admin
         $c3 = $c4 = "warning";
         switch($status) {
             case 'ok':
-                $s3 = "$ {$raD['amount']} on {$raD['date']}";
+                $s3 = "$ {$raD['amount']} on {$raD['date_db']}";
                 $c3 = $c4 = 'success';
                 break;
             case 'notfound':
                 $s1 = "Can't find donation $k";
-                $s4 = "Add donation record";
+                $s4 = "Will create donation record";
                 $c1 = $c2 = 'warning';
                 break;
             case 'notfound_nocontact':
                 $s1 = "Can't find donation $k";
-                $s2 = "contact unknown";
-                $c1 = $c2 = 'warning';
+                $s2 = "contact unknown : {$raD['raOriginal'][$this->raSheetCols['lastname']]} {$raD['raOriginal'][$this->raSheetCols['email']]}";
+                $c1 = $c2 = 'danger';
+                break;
+            case 'name_mismatch':
+                $c1 = $c2 = $c3 = 'danger';
+                $s3 = $errmsg;
                 break;
             case 'wrongamount':
             case 'wrongdate':
                 $s3 = $errmsg;
-                $s4 = "Update "
-                     .(($v = @$raD['actions']['amount']) ? "amount $ $v " : "")
-                     .(($v = @$raD['actions']['date']) ? "date $v " : "")
-                     ."on donation {$raD['actions']['kD']}";
-                break;
-                $s3 = $errmsg;
+                $s4 = "Will update "
+                     .(($v = @$raD['action']['amount']) ? "amount=\${$v} " : "")
+                     .(($v = @$raD['action']['date_db']) ? "date=$v " : "")
+                     ."on donation {$raD['action']['kD']}";
                 break;
         }
         $s .= "<tr><td class='$c1'>$s1</td><td class='$c2'>$s2</td><td class='$c3'>$s3</td><td class='$c4'>$s4</td></tr>";
@@ -190,41 +201,130 @@ class MbrDonationsTab_Admin
         /* Create two arrays raSingle  = one-time donations identified by transaction id
                              raMonthly = monthly donations collapsed to record total amount and latest date
          */
-        $raSingle = $raMonthly = [];
+        $raSingle = $raMonthly = $raIgnored = [];
 
-        /* Aggregate monthly donations to sum(amount) and max(date) for each donor
+        /* Aggregate monthly donations to sum(amount) and max(date) for each donor, per year
          * Store single donations in raSingle
          */
         foreach($raRawRows as $ra) {
+            if( $ra[$this->raSheetCols['email']] == 'ANON' ) {
+                $raIgnored[$ra[$this->raSheetCols['trid']]] = ['name'=>'ANON',
+                                                               'amount'=>$ra[$this->raSheetCols['amount']],
+                                                               'date_db'=>$this->getDateFromExcelDate($ra[$this->raSheetCols['don_date']])];
+                continue;
+            }
+            if( $ra[$this->raSheetCols['email']] == 'info@canadahelps.org' ) {  // CH deposits Cause Funds Disbursements from some anonymous source
+                $raIgnored[$ra[$this->raSheetCols['trid']]] = ['name'=>"CanHelps",
+                                                               'amount'=>$ra[$this->raSheetCols['amount']],
+                                                               'date_db'=>$this->getDateFromExcelDate($ra[$this->raSheetCols['don_date']])];
+                continue;
+            }
+
             if( ($k = trim(@$ra[$this->raSheetCols['monthly']] ??"")) ) {
                 // monthly donations
-                $k = "monthly_{$this->getYearFromExcelDate($ra['DONATION DATE'])}_$k";
-                $total_amount = floatval(@$ra['AMOUNT']) + @$raMonthly[$k]['amount'];
-                $last_date = max(@$ra['DONATION DATE'], @$raMonthly[$k]['date']);
+                $k = "monthly_{$this->getYearFromExcelDate($ra[$this->raSheetCols['don_date']])}_$k";
+                $total_amount = floatval(@$ra[$this->raSheetCols['amount']]) + @$raMonthly[$k]['amount'];
+                $last_date = max(@$ra[$this->raSheetCols['don_date']], @$raMonthly[$k]['date']);
                 $raMonthly[$k] = ['amount'=>$total_amount, 'date'=>$last_date, 'raOriginal'=>$ra];
             } else {
                 // single donations
                 $trid = @$ra[$this->raSheetCols['trid']];
-                $raSingle[$trid] = ['raOriginal'=>$ra];
-                $raSingle[$trid] = ['amount'=>$ra['AMOUNT'], 'date'=>$ra['DONATION DATE'], 'raOriginal'=>$ra];
+                $raSingle[$trid] = ['amount'=>$ra[$this->raSheetCols['amount']], 'date'=>$ra[$this->raSheetCols['don_date']], 'raOriginal'=>$ra];
             }
         }
 
-        // compare monthly donations with mbr_donations
-        foreach($raMonthly as $k => $ra) {
-            $v = addslashes($k);
-            $kfrD = $this->oMbrDb->GetKFRCond('DxM', "v1='$v'");
-            $raMonthly[$k] += $this->uploadPreprocessRecord($kfrD, $ra);
+        // compare CanHelps donations with mbr_donations. $k is the CanHelps trid/monthlyid
+        foreach($raMonthly as $k => $ra) { $raMonthly[$k] += $this->uploadPreprocessRecord($k, $ra); }
+        foreach($raSingle as $k => $ra)  { $raSingle[$k] += $this->uploadPreprocessRecord($k, $ra); }
+
+        $this->oSVA->VarSet( 'uploadedCHData', ['raRaw'=>$raRawRows, 'raSingle'=>$raSingle, 'raMonthly'=>$raMonthly, 'raIgnored'=>$raIgnored] );
+    }
+
+    private function uploadPreprocessRecord( string $k, array $raD )
+    {
+        // $raD contains date, amount, raOriginal.
+        // The output array will be merged into $raD
+        $raOut = ['status'=>"", 'M_name'=>"", 'action'=>['do'=>''],
+                  'date_db'=>$this->getDateFromExcelDate($raD['date'])
+                 ];
+
+        // look up the donation record by the CanHelps id and compare it with the CanHelps record
+        $kfrD = $this->oMbr->oDB->GetKFRCond('DxM', "v1='".addslashes($k)."'");
+        if( !$kfrD ) {
+            if( ($e = $raD['raOriginal'][$this->raSheetCols['email']]) &&
+                ($e = addslashes($e)) &&
+                ($kfrM = $this->oMbr->oDB->GetKFRCond('M', "email='$e'")) )
+            {
+                // the donation record doesn't exist but the contact does
+                $raOut['status'] = 'notfound';
+                $raOut['M_name'] = $this->oMbr->GetContactName($kfrM->Key());
+                $raOut['action'] = ['do'=>'new', 'fk_mbr_contacts'=>$kfrM->Key(), 'amount'=>$raD['amount'], 'date_db'=>$raOut['date_db']];
+            } else {
+                // no donation record and contact unknown
+                $raOut['status'] = 'notfound_nocontact';
+                $raOut['M_name'] = "";
+            }
+
+            goto done;
         }
 
-        // compare single donations with mbr_donations
-        foreach($raSingle as $k => $ra) {
-            $v = addslashes($k);
-            $kfrD = $this->oMbrDb->GetKFRCond('DxM', "v1='{$v}'");
-            $raSingle[$k] += $this->uploadPreprocessRecord($kfrD, $ra);
+        /* the donation record exists
+         */
+        $raOut['M_name'] = $this->oMbr->GetContactName($kfrD->Value('M__key'));
+
+        if( $raD['amount'] != $kfrD->Value('amount') ) {
+            $raOut['status'] = "wrongamount:CanHelps total is {$raD['amount']} but db amount is {$kfrD->Value('amount')}";
+        }
+        else
+        if( $raOut['date_db'] != $kfrD->Value('date_received') ) {
+            $raOut['status'] = "wrongdate:CanHelps date is {$raOut['date_db']} but db date is {$kfrD->Value('date_received')}";
+        }
+        else
+        if( ($n1 = trim(@$raD['raOriginal'][$this->raSheetCols['lastname']])??"") != ($n2 = trim($kfrD->Value('M_lastname'))) ) {
+            $raOut['status'] = "name_mismatch:CanHelps last name is $n1 but db last name is $n2";
+        } else {
+            $raOut['status'] = 'ok';
+        }
+        if( SEEDCore_StartsWith($raOut['status'], 'wrong') ) {
+            // wrongamount or wrongdate : update both and KF will just change the one that needs it
+            $raOut['action'] = ['do'=>'update', 'kD'=>$kfrD->Key(), 'amount'=>$raD['amount'], 'date_db'=>$raOut['date_db']];
         }
 
-        $this->oSVA->VarSet( 'uploadedCHData', ['raRaw'=>$raRawRows, 'raSingle'=>$raSingle, 'raMonthly'=>$raMonthly] );
+        done:
+        return( $raOut );
+    }
+
+    private function uploadDoCommit()
+    {
+        $s = "";
+
+        $raCHData = $this->oSVA->VarGet('uploadedCHData');
+        foreach(array_merge($raCHData['raMonthly'], $raCHData['raSingle']) as $k => $raD) {
+            $bIsMonthly = SEEDCore_StartsWith($k, 'monthly');
+
+            if( $raD['action']['do'] == 'update' && ($kfrD = $this->oMbr->oDB->GetKFR('D', $raD['action']['kD'])) ) {  // should only happen for monthly
+                // wrongamount or wrongdate
+                // commit changes to amount and date (this happens every time there's another monthly donation
+                if( ($v = @$raD['action']['amount']) )   $kfrD->SetValue('amount', $v);
+                if( ($v = @$raD['action']['date_db']) )  $kfrD->SetValue('date_received', $v);
+                $kfrD->PutDBRow();
+            }
+
+            if( @$raD['action']['do'] == 'new' && ($kfrD = $this->oMbr->oDB->KFRel('D')->CreateRecord()) ) {
+                // notfound
+                $kfrD->SetValue('fk_mbr_contacts', $raD['action']['fk_mbr_contacts']);
+                $kfrD->SetValue('amount',          $raD['action']['amount']);
+                $kfrD->SetValue('date_received',   $raD['action']['date_db']);
+                $kfrD->SetValue('receipt_num',     -3);     // CanadaHelps
+
+                $kfrD->SetValue('v1',              $k);
+                $kfrD->SetValue('notes',           $bIsMonthly ? ("CH monthly total for ".substr($raD['action']['date_db'], 0, 4)) : "CH donation $k");
+                $kfrD->SetNull('date_issued');  // DATE type requires NULL if not specified
+                $kfrD->PutDBRow();
+            }
+        }
+
+        return($s);
     }
 
     private function getDateFromExcelDate( $date )
@@ -237,79 +337,5 @@ class MbrDonationsTab_Admin
     private function getYearFromExcelDate( $date )
     {
         return( $date ? substr($this->getDateFromExcelDate($date), 0, 4) : "" );
-    }
-
-    private function uploadPreprocessRecord( ?KeyframeRecord $kfrD, array $raD )
-    {
-        $raOut = ['status'=>"", 'M_name'=>"", 'actions'=>[]];
-
-        $raD['date'] = $this->getDateFromExcelDate($raD['date']);
-
-        if( !$kfrD ) {
-            $raOut['status'] = 'notfound_nocontact';
-            $raOut['M_name'] = "";
-
-            if( ($e = $raD['raOriginal'][$this->raSheetCols['email']]) ) {
-                $e = addslashes($e);
-                if( ($kfrM = $this->oMbrDb->GetKFRCond('M', "email='$e'")) ) {
-                    $raOut['status'] = 'notfound';
-                    $raOut['M_name'] = (new Mbr_Contacts($this->oApp))->GetContactName($kfrM->Key());
-
-                    // the donation record doesn't exist but the contact does
-                    $raOut['actions'] = ['new'=>true, 'fk_mbr_contacts'=>$kfrM->Key(), 'amount'=>$raD['amount'], 'date'=>$raD['date']];
-                }
-            }
-            goto done;
-        }
-
-        $raOut['M_name'] = (new Mbr_Contacts($this->oApp))->GetContactName($kfrD->Value('M__key'));
-
-        if( $raD['amount'] != $kfrD->Value('amount') ) {
-            $raOut['status'] = "wrongamount:CanHelps total is {$raD['amount']} but db amount is {$kfrD->Value('amount')}";
-            $raOut['actions']['kD'] = $kfrD->Key();
-            $raOut['actions']['amount'] = $raD['amount'];
-        } else
-        if( $raD['date'] != $kfrD->Value('date_received') ) {
-            $raOut['status'] = "wrongdate:CanHelps date is {$raD['date']} but db date is {$kfrD->Value('date_received')}";
-            $raOut['actions']['kD'] = $kfrD->Key();
-            $raOut['actions']['date'] = $raD['date'];
-        } else {
-            $raOut['status'] = 'ok';
-        }
-
-        done:
-        return( $raOut );
-    }
-
-    private function uploadDoCommit()
-    {
-        $s = "";
-
-        $raCHData = $this->oSVA->VarGet('uploadedCHData');
-        foreach($raCHData['raMonthly'] as $k => $raD) {
-            // commit changes to amount and date (this happens every time there's another monthly donation
-            if( @$raD['actions']['kD'] && ($kfrD = $this->oMbrDb->GetKFR('D', $raD['actions']['kD'])) ) {
-                if( ($v = @$raD['actions']['amount']) )  $kfrD->SetValue('amount', $v);
-                if( ($v = @$raD['actions']['date']) )    $kfrD->SetValue('date_received', $v);
-                $kfrD->PutDBRow();
-            }
-
-            // add a new monthly donation
-            if( @$raD['actions']['new'] && ($kfrD = $this->oMbrDb->KFRel('D')->CreateRecord()) ) {
-                $kfrD->SetValue('fk_mbr_contacts', $raD['actions']['fk_mbr_contacts']);
-                $kfrD->SetValue('amount',          $raD['actions']['amount']);
-                $kfrD->SetValue('date_received',   $raD['actions']['date']);
-                $kfrD->SetValue('receipt_num',     -3);     // CanadaHelps
-
-                $kfrD->SetValue('v1',              $k);
-                $kfrD->SetValue('notes',           "CH monthly total for ".substr($raD['actions']['date'], 0, 4));
-
-                $kfrD->SetNull('date_issued');
-
-                $kfrD->PutDBRow();
-            }
-        }
-
-        return($s);
     }
 }
